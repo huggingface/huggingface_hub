0833efa48cee5b904671af6159e15ab6fedd5269
Julien Chaumond <julien@huggingface.co>
2020-12-23 10:27:00 +0100

    GitHub Action to release on PyPI (#2)
    * GitHub Action to release on PyPI
    
    * Update python-tests.yml
    
    * Update python-release.yml

--- src/huggingface_hub/__init__.py
+++ src/huggingface_hub/__init__.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 
 from .file_download import HUGGINGFACE_CO_URL_TEMPLATE, cached_download, hf_hub_url
 from .hf_api import HfApi, HfFolder
