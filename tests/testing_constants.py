1e351bc3ff4c99cf72d7148f84d76ee387cd2397
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2021-02-20 19:53:27 +0100

    Created HF Repository class for model hub (#7)
    * Created HF Repository class for model hub
    
    * fixed issues with pushing to hub from existing files
    
    * Update HfRepostiory.py
    
    * Update HfRepostiory.py
    
    * refactored HFRepository
    
    * removed model_card
    
    * fixed push to hub
    
    * make style
    
    * added comments
    
    * fixed remote user user
    
    * doc
    
    * rename this as not a lot (if any) is going to be HF-specific
    
    * black
    
    * Update doc with the philosophy of the class
    
    * work-in-progress (reorganize)
    
    * Still work-in-progress, but this is starting to take shape
    
    * Fix stuff
    
    Co-authored-by: Julien Chaumond <julien@huggingface.co>

---
+++ tests/testing_constants.py
@@ -0,0 +1,5 @@
+USER = "__DUMMY_TRANSFORMERS_USER__"
+PASS = "__DUMMY_TRANSFORMERS_PASS__"
+
+ENDPOINT_STAGING = "https://moon-staging.huggingface.co"
+ENDPOINT_STAGING_BASIC_AUTH = f"https://{USER}:{PASS}@moon-staging.huggingface.co"
