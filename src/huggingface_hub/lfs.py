833d70347b8ee216466b777b9dc722726d19639d
Julien Chaumond <julien@huggingface.co>
2021-02-23 19:23:29 +0100

    Add utility to install git-lfs in userspace on Linux

--- src/huggingface_hub/lfs.py
+++ src/huggingface_hub/lfs.py
@@ -13,5 +13,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import subprocess
+import sys
+
 
 LFS_MULTIPART_UPLOAD_COMMAND = "lfs-multipart-upload"
+
+
+def install_lfs_in_userspace():
+    """
+    If in Linux, installs git-lfs in userspace
+    (sometimes useful if you can't `sudo apt install` or equivalent).
+    """
+    if sys.platform != "linux":
+        raise ValueError("Only implemented for Linux right now")
+    GIT_LFS_TARBALL = "https://github.com/git-lfs/git-lfs/releases/download/v2.13.1/git-lfs-linux-amd64-v2.13.1.tar.gz"
+    CWD = os.path.join(os.getcwd(), "install_lfs")
+    os.makedirs(CWD, exist_ok=True)
+    subprocess.run(
+        ["wget", "-O", "tarball.tar.gz", GIT_LFS_TARBALL], check=True, cwd=CWD
+    )
+    subprocess.run(["tar", "-xvzf", "tarball.tar.gz"], check=True, cwd=CWD)
+    subprocess.run(["bash", "install.sh"], check=True, cwd=CWD)
