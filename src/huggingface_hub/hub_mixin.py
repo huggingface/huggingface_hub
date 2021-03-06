6e9b4b903ca5e1cdb26d654c3cd478d2d0ceb630
VasudevGupta7 <7vasudevgupta@gmail.com>
2021-03-07 00:21:15 +0530

    add everything

--- src/huggingface_hub/hub_mixin.py
+++ src/huggingface_hub/hub_mixin.py
@@ -19,47 +19,48 @@ class ModelHubMixin(object):
             >>> from huggingface_hub import ModelHubMixin
 
             >>> class MyModel(nn.Module, ModelHubMixin):
-            ...    def __init__(self):
+            ...    def __init__(self, **kwargs):
             ...        super().__init__()
+            ...        self.config = kwargs.pop("config", None)
             ...        self.layer = ...
             ...    def forward(self, ...)
             ...        return ...
 
             >>> model = MyModel()
-            >>> model.save_pretrained("mymodel") # Saving model weights in the directory
-            >>> model.upload_to_hub("mymodel", "model-1") # Pushing model-weights to hf-hub
+            >>> model.save_pretrained("mymodel", push_to_hub=False) # Saving model weights in the directory
+            >>> model.push_to_hub("mymodel", "model-1") # Pushing model-weights to hf-hub
 
             >>> # Downloading weights from hf-hub & model will be initialized from those weights
-            >>> model = MyModel.from_pretrained("username/mymodel")
+            >>> model = MyModel.from_pretrained("username/mymodel@main")
         """
 
     def save_pretrained(
-        self, save_directory: str, config: dict = None, push_to_hub=False, model_id=None
+        self,
+        save_directory: str,
+        config: dict = None,
+        push_to_hub: bool = False,
+        model_id: str = None,
     ):
         """
         Saving weights in local directory.
 
         Parameters:
             save_directory (:obj:`str`):
-                Directory in which you want to save weights.
+                Specify directory in which you want to save weights.
             config (:obj:`dict`, `optional`):
-                specify config incase you want to save config.
+                specify config (must be dict) incase you want to save it.
+            push_to_hub (:obj:`bool`, `optional`, defaults to :obj:`False`):
+                Set it to `True` in case you want to push your weights to huggingface_hub
+            model_id (:obj:`str`, `optional`, defaults to :obj:`save_directory`):
+                Repo name in huggingface_hub. If not specified, repo name will be same as `save_directory`
         """
         os.makedirs(save_directory, exist_ok=True)
-        config_to_save = None
 
         # saving config
-        if hasattr(self, "config"):
-            if isinstance(self.config, dict):
-                config_to_save = self.config
-
         if isinstance(config, dict):
-            config_to_save = config
-
-        if config_to_save is not None:
             path = os.path.join(save_directory, CONFIG_NAME)
             with open(path, "w") as f:
-                json.dump(config_to_save, f)
+                json.dump(config, f)
 
         # saving model weights
         path = os.path.join(save_directory, PYTORCH_WEIGHTS_NAME)
@@ -69,7 +70,7 @@ class ModelHubMixin(object):
             model_id = save_directory
 
         if push_to_hub:
-            self.push_to_hub(save_directory, model_id)
+            return self.push_to_hub(save_directory, model_id=model_id)
 
     def _save_pretrained(self, path):
         """
@@ -79,7 +80,7 @@ class ModelHubMixin(object):
         torch.save(model_to_save.state_dict(), path)
 
     @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path: str, *model_args, **kwargs):
+    def from_pretrained(cls, pretrained_model_name_or_path: str, **kwargs):
         r"""
         Instantiate a pretrained pytorch model from a pre-trained model configuration from huggingface-hub.
         The model is set in evaluation mode by default using ``model.eval()`` (Dropout modules are deactivated). To
@@ -133,18 +134,12 @@ class ModelHubMixin(object):
         cache_dir = kwargs.pop("cache_dir", None)
         local_files_only = kwargs.pop("local_files_only", False)
 
-        if len(model_id.split("/")) == 1:
-            name = model_id
-        else:
-            _, name = model_id.split("/")
+        revision = None
+        if len(model_id.split("@")) == 2:
+            model_id, revision = model_id.split("@")
 
-        revision = "main"
-        if len(name.split("@")) > 1:
-            name, revision = name.split("@")
-
-        if name in os.listdir() and CONFIG_NAME in os.listdir(name):
-            print("LOADING weights from local directory")
-            config_file = os.path.join(name, CONFIG_NAME)
+        if model_id in os.listdir() and CONFIG_NAME in os.listdir(model_id):
+            config_file = os.path.join(model_id, CONFIG_NAME)
         else:
             try:
                 config_url = hf_hub_url(
@@ -162,9 +157,9 @@ class ModelHubMixin(object):
             except:
                 config_file = None
 
-        if name in os.listdir():
+        if model_id in os.listdir():
             print("LOADING weights from local directory")
-            model_file = os.path.join(name, PYTORCH_WEIGHTS_NAME)
+            model_file = os.path.join(model_id, PYTORCH_WEIGHTS_NAME)
         else:
             model_url = hf_hub_url(
                 model_id, filename=PYTORCH_WEIGHTS_NAME, revision=revision
@@ -182,9 +177,9 @@ class ModelHubMixin(object):
         if config_file is not None:
             with open(config_file, "r", encoding="utf-8") as f:
                 config = json.load(f)
-            model = cls(config, **kwargs)
-        else:
-            model = cls(**kwargs)
+            kwargs.update({"config": config})
+
+        model = cls(**kwargs)
 
         state_dict = torch.load(model_file, map_location=map_location)
         model.load_state_dict(state_dict, strict=strict)
@@ -193,22 +188,27 @@ class ModelHubMixin(object):
         return model
 
     @staticmethod
-    def push_to_hub(weights_directory: str, model_id: str, **kwargs):
+    def push_to_hub(save_directory: str, **kwargs):
         """
         Parameters:
-            weights_directory (:obj:`Union[str, os.PathLike]`):
-                Directory having model-weights & config.
-            model_id is like ``bert-base-uncased@main``
+            save_directory (:obj:`Union[str, os.PathLike]`):
+                Directory having model weights & config.
+            model_id (:obj:`str`, `optional`, defaults to :obj:`save_directory`):
+                Repo name in huggingface_hub. If not specified, repo name will be same as `save_directory`
+            repo_url (:obj:`str`, `optional`):
+                Specify this in case you want to push to existing repo in hub.
+            organization (:obj:`str`, `optional`):
+                Organization in which you want to push your model.
+            private (:obj:`bool`, `optional`):
+                private: Whether the model repo should be private (requires a paid huggingface.co account)
+            commit_message (:obj:`str`, `optional`, defaults to :obj:`add model`):
+                Message to commit while pushing
         """
         repo_url = kwargs.pop("repo_url", None)
-
         commit_message = kwargs.pop("commit_message", "add model")
         organization = kwargs.pop("organization", None)
         private = kwargs.pop("private", None)
-
-        revision = "main"
-        if len(model_id.split("@")) > 1:
-            model_id, revision = model_id.split("@")
+        model_id = kwargs.pop("model_id", save_directory)
 
         token = HfFolder.get_token()
         if repo_url is None:
@@ -221,6 +221,6 @@ class ModelHubMixin(object):
                 exist_ok=True,
             )
 
-        repo = Repository(weights_directory, clone_from=repo_url, use_auth_token=token)
+        repo = Repository(save_directory, clone_from=repo_url, use_auth_token=token)
 
         return repo.push_to_hub(commit_message=commit_message)
---
+++ tests/test_hubmixin.py
@@ -0,0 +1,51 @@
+import unittest
+
+import torch.nn as nn
+
+from huggingface_hub import ModelHubMixin
+
+
+HUGGINGFACE_ID = "vasudevgupta"
+DUMMY_REPO_NAME = "dummy"
+
+
+class DummyModel(nn.Module, ModelHubMixin):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.config = kwargs.pop("config", None)
+        self.l1 = nn.Linear(2, 2)
+
+    def forward(self, x):
+        return self.l1(x)
+
+
+class DummyModelTest(unittest.TestCase):
+    def test_save_pretrained(self):
+        model = DummyModel()
+        _ = model.save_pretrained(DUMMY_REPO_NAME)
+        _ = model.save_pretrained(
+            DUMMY_REPO_NAME, config={"num": 12, "act": "gelu"}, push_to_hub=True
+        )
+        _ = model.save_pretrained(
+            DUMMY_REPO_NAME, config={"num": 24, "act": "relu"}, push_to_hub=True
+        )
+        _ = model.save_pretrained(
+            "dummy-wts", config=None, push_to_hub=True, model_id=DUMMY_REPO_NAME
+        )
+
+    def test_from_pretrained(self):
+        model = DummyModel()
+        _ = model.save_pretrained(
+            DUMMY_REPO_NAME, config={"num": 7, "act": "gelu_fast"}, push_to_hub=True
+        )
+
+        model = DummyModel.from_pretrained(f"{HUGGINGFACE_ID}/{DUMMY_REPO_NAME}@main")
+        self.assertTrue(model.config == {"num": 7, "act": "gelu_fast"})
+
+    def test_push_to_hub(self):
+        model = DummyModel()
+        _ = model.save_pretrained("dummy-wts", push_to_hub=False)
+        model.push_to_hub("dummy-wts", model_id=DUMMY_REPO_NAME)
+
+if __name__ == '__main__':
+    DummyModelTest().test_save_pretrained()
