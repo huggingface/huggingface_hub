cfd67fe9b4f658a5717bef350aecc4d3851428e6
theo <theo@matussie.re>
2021-03-26 14:24:08 +0100

    import methods from datasets + refactor

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -15,15 +15,16 @@
 
 
 import os
+from http import HTTPStatus
 from os.path import expanduser
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Literal, Optional, Tuple, Union
 
 import requests
 
 from .constants import REPO_TYPES
 
 
-ENDPOINT = "https://huggingface.co"
+ENDPOINT = "https://huggingface.co/api/"
 
 
 class RepoObj:
@@ -34,6 +35,41 @@ class RepoObj:
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
             setattr(self, k, v)
+
+
+class ObjectInfo:
+    """
+    Info about a public dataset or Metric accessible from our S3.
+    """
+
+    def __init__(
+        self,
+        id: str,
+        key: str,
+        lastModified: Optional[str] = None,
+        description: Optional[str] = None,
+        citation: Optional[str] = None,
+        size: Optional[int] = None,
+        etag: Optional[str] = None,
+        siblings: List[Dict] = None,
+        author: str = None,
+        **kwargs,
+    ):
+        self.id = id  # id of dataset
+        self.key = key  # S3 object key of config.json
+        self.lastModified = lastModified
+        self.description = description
+        self.citation = citation
+        self.size = size
+        self.etag = etag
+        self.siblings = siblings  # list of files that constitute the dataset
+        self.author = author
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __repr__(self):
+        single_line_description = self.description.replace("\n", "") if self.description is not None else ""
+        return f"datasets.ObjectInfo(\n\tid='{self.id}',\n\tdescription='{single_line_description}',\n\tfiles={self.siblings}\n)"
 
 
 class ModelSibling:
@@ -57,24 +93,36 @@ class ModelInfo:
         modelId: Optional[str] = None,  # id of model
         tags: List[str] = [],
         pipeline_tag: Optional[str] = None,
-        siblings: Optional[
-            List[Dict]
-        ] = None,  # list of files that constitute the model
-        **kwargs
+        siblings: Optional[List[Dict]] = None,  # list of files that constitute the model
+        **kwargs,
     ):
         self.modelId = modelId
         self.tags = tags
         self.pipeline_tag = pipeline_tag
-        self.siblings = (
-            [ModelSibling(**x) for x in siblings] if siblings is not None else None
-        )
+        self.siblings = [ModelSibling(**x) for x in siblings] if siblings is not None else None
         for k, v in kwargs.items():
             setattr(self, k, v)
 
 
 class HfApi:
     def __init__(self, endpoint=None):
-        self.endpoint = endpoint if endpoint is not None else ENDPOINT
+        self.endpoint: str = endpoint if endpoint is not None else ENDPOINT
+        self.token: Optional[str] = None
+
+    @property
+    def auth_headers(self):
+        if self.token is None:
+            raise ValueError("you need to login before using this -> `.login(username, password)`")
+        return dict(authorization=f"Bearer {self.token}")
+
+    def _api(
+        self, path: str, method: Literal["get", "post"] = "get", ignore_status: List[int] = None, **kwargs
+    ) -> Union[Dict, List]:
+        response = requests.request(method, f"{self.endpoint}/{path}", **kwargs)
+        if ignore_status is not None and response.status_code in ignore_status:
+            return response.json()
+        response.raise_for_status()
+        return response.json()
 
     def login(self, username: str, password: str) -> str:
         """
@@ -84,60 +132,69 @@ class HfApi:
 
         Throws: requests.exceptions.HTTPError if credentials are invalid
         """
-        path = "{}/api/login".format(self.endpoint)
-        r = requests.post(path, json={"username": username, "password": password})
-        r.raise_for_status()
-        d = r.json()
-        return d["token"]
+        response = self._api("login", "post", json=dict(username=username, password=password))
+        self.token = response["token"]
+        return response["token"]
 
-    def whoami(self, token: str) -> Tuple[str, List[str]]:
+    def whoami(self) -> Tuple[str, List[str]]:
         """
         Call HF API to know "whoami"
         """
-        path = "{}/api/whoami".format(self.endpoint)
-        r = requests.get(path, headers={"authorization": "Bearer {}".format(token)})
-        r.raise_for_status()
-        d = r.json()
-        return d["user"], d["orgs"]
+        response = self._api("whoami", headers=self.auth_headers)
+        return response["user"], response["orgs"]
 
-    def logout(self, token: str) -> None:
+    def logout(self) -> None:
         """
         Call HF API to log out.
         """
-        path = "{}/api/logout".format(self.endpoint)
-        r = requests.post(path, headers={"authorization": "Bearer {}".format(token)})
-        r.raise_for_status()
+        self._api("logout", "post", headers=self.auth_headers)
 
     def model_list(self) -> List[ModelInfo]:
         """
         Get the public list of all the models on huggingface.co
         """
-        path = "{}/api/models".format(self.endpoint)
-        r = requests.get(path)
-        r.raise_for_status()
-        d = r.json()
-        return [ModelInfo(**x) for x in d]
+        response = self._api("models")
+        return [ModelInfo(**x) for x in response]
 
-    def list_repos_objs(
-        self, token: str, organization: Optional[str] = None
-    ) -> List[RepoObj]:
+    def dataset_list(self, with_community_datasets=True, id_only=False) -> Union[List[ObjectInfo], List[str]]:
+        """
+        Get the public list of all the datasets on huggingface, including the community datasets
+        """
+        response = self._api("datasets")
+        datasets = [ObjectInfo(**x) for x in response]
+        if not with_community_datasets:
+            datasets = [d for d in datasets if "/" not in d.id]
+        if id_only:
+            datasets = [d.id for d in datasets]
+        return datasets
+
+    def metric_list(self, with_community_metrics=True, id_only=False) -> Union[List[ObjectInfo], List[str]]:
+        """
+        Get the public list of all the metrics on huggingface, including the community metrics
+        """
+        response = self._api("metrics")
+        metrics = [ObjectInfo(**x) for x in response]
+        if not with_community_metrics:
+            metrics = [m for m in metrics if "/" not in m.id]
+        if id_only:
+            metrics = [m.id for m in metrics]
+        return metrics
+
+    def list_repos_objs(self, organization: Optional[str] = None) -> List[RepoObj]:
         """
         HuggingFace git-based system, used for models.
 
         Call HF API to list all stored files for user (or one of their organizations).
         """
-        path = "{}/api/repos/ls".format(self.endpoint)
-        params = {"organization": organization} if organization is not None else None
-        r = requests.get(
-            path, params=params, headers={"authorization": "Bearer {}".format(token)}
+        response = self._api(
+            "repos/ls",
+            headers=self.auth_headers,
+            params=dict(organization=organization) if organization is not None else None,
         )
-        r.raise_for_status()
-        d = r.json()
-        return [RepoObj(**x) for x in d]
+        return [RepoObj(**x) for x in response]
 
     def create_repo(
         self,
-        token: str,
         name: str,
         organization: Optional[str] = None,
         private: Optional[bool] = None,
@@ -162,8 +219,6 @@ class HfApi:
         Returns:
             URL to the newly created repo.
         """
-        path = "{}/api/repos/create".format(self.endpoint)
-
         if repo_type not in REPO_TYPES:
             raise ValueError("Invalid repo type")
 
@@ -172,21 +227,16 @@ class HfApi:
             json["type"] = repo_type
         if lfsmultipartthresh is not None:
             json["lfsmultipartthresh"] = lfsmultipartthresh
-        r = requests.post(
-            path,
-            headers={"authorization": "Bearer {}".format(token)},
+        response = self._api(
+            "repos/create",
+            ignore_status=[HTTPStatus.CONFLICT] if exist_ok else 0,
+            headers=self.auth_headers,
             json=json,
         )
-        if exist_ok and r.status_code == 409:
-            d = r.json()
-            return d["url"]
-        r.raise_for_status()
-        d = r.json()
-        return d["url"]
+        return response["url"]
 
     def delete_repo(
         self,
-        token: str,
         name: str,
         organization: Optional[str] = None,
         repo_type: Optional[str] = None,
@@ -207,12 +257,7 @@ class HfApi:
         if repo_type is not None:
             json["type"] = repo_type
 
-        r = requests.delete(
-            path,
-            headers={"authorization": "Bearer {}".format(token)},
-            json=json,
-        )
-        r.raise_for_status()
+        self._api("repos/delete", headers=self.auth_headers, json=json)
 
 
 class HfFolder:
