54d4bf92e072a73606315de934ddeb4de71594be
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-04-07 19:56:45 -0400

    0.0.8

--- src/huggingface_hub/__init__.py
+++ src/huggingface_hub/__init__.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from .constants import (
     CONFIG_NAME,
