e1c56463121599bc6575741e4c78cf17f0ef8b21
Julien Chaumond <chaumond@gmail.com>
2021-05-21 10:37:48 +0200

    initial import
    commit imported from https://github.com/huggingface/widgets/commit/367c4cf89b70a7c73cb8d772117559b64abf3a16

---
+++ interfaces/Types.ts
@@ -0,0 +1,13 @@
+
+/// Order by decreasing specificity
+export enum PipelineType {
+	"text-classification" = "text-classification",
+	"token-classification" = "token-classification",
+	"question-answering" = "question-answering",
+	"translation" = "translation",
+	"summarization" = "summarization",
+	"text-generation" = "text-generation",
+	"fill-mask" = "fill-mask",
+}
+
+export const ALL_PIPELINE_TYPES = Object.keys(PipelineType) as (keyof typeof PipelineType)[];
