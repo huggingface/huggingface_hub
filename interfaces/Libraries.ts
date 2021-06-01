2bb58b92720a27611a4e8a3e6ac7b3f9ded9c39f
osanseviero <osanseviero@gmail.com>
2021-06-01 17:17:14 +0200

    change order of spacy code snippet

--- interfaces/Libraries.ts
+++ interfaces/Libraries.ts
@@ -176,13 +176,14 @@ model = SentenceTransformer("${model.modelId}")`;
 const spacy = (model: ModelData) =>
 `!pip install https://huggingface.co/${model.modelId}/resolve/main/${nameWithoutNamespace(model.modelId)}-any-py3-none-any.whl
 
-# Importing as module.
-import ${nameWithoutNamespace(model.modelId)}
-nlp = ${nameWithoutNamespace(model.modelId)}.load()
-
 # Using spaCy.load().
 import spacy
-nlp = spacy.load("${nameWithoutNamespace(model.modelId)}")`;
+nlp = spacy.load("${nameWithoutNamespace(model.modelId)}")
+
+# Importing as module.
+import ${nameWithoutNamespace(model.modelId)}
+nlp = ${nameWithoutNamespace(model.modelId)}.load()`;
+
 
 const transformers = (model: ModelData) =>
 `from transformers import AutoTokenizer, ${model.autoArchitecture}
