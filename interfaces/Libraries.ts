664a34354726b9d7a6e285c0a0d32cf7df1ce636
Pete <epwalsh10@gmail.com>
2021-06-07 09:45:03 -0700

    Fix AllenNLP QA example and update Inference API to use main repo
    * fix AllenNLP QA example
    
    * update AllenNLP, AllenNLP Models

--- interfaces/Libraries.ts
+++ interfaces/Libraries.ts
@@ -89,7 +89,7 @@ const allennlpQuestionAnswering = (model: ModelData) =>
 `import allennlp_models
 from allennlp.predictors.predictor import Predictor
 
-predictor = Predictor.from_path("${model.modelId}")
+predictor = Predictor.from_path("hf://${model.modelId}")
 predictor_input = {"passage": "My name is Wolfgang and I live in Berlin", "question": "Where do I live?"}
 predictions = predictor.predict_json(predictor_input)`;
 
