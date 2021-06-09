f0f047886c909afd243fe286fc5ac3a0c9004f1f
Julien Chaumond <julien@huggingface.co>
2021-06-09 11:08:49 +0200

    svelte: set typescript as default

--- widgets/svelte.config.js
+++ widgets/svelte.config.js
@@ -4,7 +4,9 @@ import preprocess from "svelte-preprocess";
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		defaults: { script: "typescript" },
+	}),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
