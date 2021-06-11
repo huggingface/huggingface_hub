f65f65133126b8a583ac235214bbd25d62235975
osanseviero <osanseviero@gmail.com>
2021-06-11 17:57:58 +0200

    Add allowlist and change to cloudpickle

--- api-inference-community/docker_images/sklearn/app/pipelines/structured_data_classification.py
+++ api-inference-community/docker_images/sklearn/app/pipelines/structured_data_classification.py
@@ -1,23 +1,34 @@
 from typing import Dict, List, Union
 
-import joblib
-import numpy as np
-import pandas as pd
+import cloudpickle
 from app.pipelines import Pipeline
 from huggingface_hub import cached_download, hf_hub_url
 
 
-DEFAULT_FILENAME = "sklearn_model.joblib"
+ALLOWLIST: List[str] = ["scikit-learn-examples"]
+DEFAULT_FILENAME = "sklearn_model.pickle"
 
 
 class StructuredDataClassificationPipeline(Pipeline):
     def __init__(self, model_id: str):
         # TODO: Obtain expected column names from repo.
-        self.model = joblib.load(
-            cached_download(hf_hub_url(model_id, DEFAULT_FILENAME))
+        # TODO: Add to model info if it's sklearn_model.pickle" (default) or some other name.
+
+        full_model_path = model_id.split("/")
+        if len(full_model_path) != 2:
+            raise ValueError(
+                f"Invalid model_id: {model_id}. It should have a namespace (:namespace:/:model_name:)"
+            )
+        namespace, model_name = full_model_path
+        if namespace not in ALLOWLIST:
+            raise ValueError(
+                f"Invalid namespace {namespace}. It should be in user/organization allowlist"
+            )
+
+        self.model = cloudpickle.load(
+            open(cached_download(hf_hub_url(model_id, DEFAULT_FILENAME)), "rb")
         )
 
-    # Dict[str, Union[List[str], List[List[Union[str, float]]]]]
     def __call__(
         self, inputs: Dict[str, Union[List[str], List[List[Union[str, float]]]]]
     ) -> List[Union[str, float]]:
