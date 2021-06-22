85f94e88940e2762dfcf7d6306c37c00005b2dee
osanseviero <osanseviero@gmail.com>
2021-06-22 16:52:19 +0200

    Change API contract for sklearn

--- api-inference-community/docker_images/sklearn/app/main.py
+++ api-inference-community/docker_images/sklearn/app/main.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import os
 from typing import Dict, Type
@@ -5,6 +6,8 @@ from typing import Dict, Type
 from api_inference_community.routes import pipeline_route, status_ok
 from app.pipelines import Pipeline, StructuredDataClassificationPipeline
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.gzip import GZipMiddleware
 from starlette.routing import Route
 
 
@@ -35,7 +38,10 @@ ALLOWED_TASKS: Dict[str, Type[Pipeline]] = {
 }
 
 
-def get_pipeline(task: str, model_id: str) -> Pipeline:
+@functools.lru_cache()
+def get_pipeline() -> Pipeline:
+    task = os.environ["TASK"]
+    model_id = os.environ["MODEL_ID"]
     if task not in ALLOWED_TASKS:
         raise EnvironmentError(f"{task} is not a valid pipeline for model : {model_id}")
     return ALLOWED_TASKS[task](model_id)
@@ -46,13 +52,20 @@ routes = [
     Route("/{whatever:path}", pipeline_route, methods=["POST"]),
 ]
 
-app = Starlette(routes=routes)
+middleware = [Middleware(GZipMiddleware, minimum_size=1000)]
 if os.environ.get("DEBUG", "") == "1":
     from starlette.middleware.cors import CORSMiddleware
 
-    app.add_middleware(
-        CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"]
+    middleware.append(
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_headers=["*"],
+            allow_methods=["*"],
+        )
     )
+
+app = Starlette(routes=routes, middleware=middleware)
 
 
 @app.on_event("startup")
@@ -62,13 +75,18 @@ async def startup_event():
     handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
     logger.handlers = [handler]
 
-    task = os.environ["TASK"]
-    model_id = os.environ["MODEL_ID"]
-    app.pipeline = get_pipeline(task, model_id)
+    # Link between `api-inference-community` and framework code.
+    app.get_pipeline = get_pipeline
+    try:
+        get_pipeline()
+    except Exception:
+        # We can fail so we can show exception later.
+        pass
 
 
 if __name__ == "__main__":
-    task = os.environ["TASK"]
-    model_id = os.environ["MODEL_ID"]
-
-    get_pipeline(task, model_id)
+    try:
+        get_pipeline()
+    except Exception:
+        # We can fail so we can show exception later.
+        pass
