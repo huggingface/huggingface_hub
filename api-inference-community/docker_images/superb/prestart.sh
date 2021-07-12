7bdd5e937eacf730b0b0436fe8638fdc8113c881
Omar Sanseviero <osanseviero@gmail.com>
2021-07-12 13:10:37 +0200

    Add logic to install requirements and clone repo

--- api-inference-community/docker_images/superb/prestart.sh
+++ api-inference-community/docker_images/superb/prestart.sh
@@ -1,1 +1,12 @@
+git clone "https://huggingface.co/${MODEL_ID}"
+
+# Get repo name: osanseviero/my-model -> my-model, clone requirements
+# and move to corresponding directory.
+REPO_NAME=${MODEL_ID#*/}  
+REQUIREMENTS="${REPO_NAME}/requirements.txt"
+if [ -f "${REQUIREMENTS}" ]; then
+    pip install -r "${REQUIREMENTS}"
+fi
+mv "${REPO_NAME}" app/pipelines/code
+
 python app/main.py
