aa5a0c3b1dd06b6a851b79f4797388fc0ac7f8e7
Omar Sanseviero <osanseviero@gmail.com>
2021-07-28 14:25:31 +0200

    Release v0.0.15

--- src/huggingface_hub/__init__.py
+++ src/huggingface_hub/__init__.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 
 from .constants import (
     CONFIG_NAME,
