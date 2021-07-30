9337be27491a92f9ec2f0029478057fe99d23643
Nicolas Patry <patry.nicolas@protonmail.com>
2021-07-30 08:27:47 +0200

    Trying to make batching part of the lib for easier modifications.

---
+++ api-inference-community/api_inference_community/batch.py
@@ -0,0 +1,59 @@
+import io
+import os
+import json
+
+import datasets
+import tqdm
+from app.main import get_pipeline
+from huggingface_hub import HfApi
+
+
+def iterate(pipe, dataset, f):
+    for i, result in enumerate(tqdm.tqdm(pipe.iter(dataset))):
+        write_result(result, f)
+
+
+def iterate_slow(pipe, dataset, f):
+    for i, item in enumerate(tqdm.tqdm(dataset)):
+        try:
+            result = pipe(item)
+        except Exception as e:
+            result = {"error": str(e)}
+
+        write_result(result, f)
+
+
+def write_result(result, f):
+    f.write(json.dumps(result))
+    f.write("\n")
+
+
+def batch(
+    dataset_name: str,
+    dataset_config: str,
+    dataset_split: str,
+    dataset_column: str,
+    token: str,
+    repo_id: str,
+    pipeline,
+):
+    dset = datasets.load_dataset(dataset_name, name=dataset_config, split=dataset_split)
+
+    f = io.BytesIO()
+    filename = f"data_{dataset_config}_{dataset_split}_{dataset_column}.txt"
+    # TODO change to .iter(...) to get max performance on GPUs
+    print("Start batch")
+
+    if hasattr(pipeline, "iter"):
+        iterate(pipeline, dset[dataset_column], f)
+    else:
+        iterate_slow(pipeline, dset[dataset_column], f)
+
+    f.seek(0)
+
+    api = HfApi()
+    repo_id = repo_id
+    try:
+        api.upload_file(token, f, filename, repo_id, repo_type="dataset")
+    except Exception:
+        pass
--- api-inference-community/docker_images/common/app/batch.py
+++ api-inference-community/docker_images/common/app/batch.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
-import io
-import os
 
-import datasets
-import tqdm
+from api_inference_community.batch import batch
 from app.main import get_pipeline
-from huggingface_hub import HfApi
-
+import os
 
 DATASET_NAME = os.getenv("DATASET_NAME")
 DATASET_CONFIG = os.getenv("DATASET_CONFIG", None)
@@ -16,48 +12,14 @@ USE_GPU = os.getenv("USE_GPU", "0").lower() in {"1", "true"}
 TOKEN = os.getenv("TOKEN")
 REPO_ID = os.getenv("REPO_ID")
 
-
-def iterate(pipe, dataset, f):
-    for i, result in enumerate(tqdm.tqdm(pipe.iter(dataset))):
-        f.write(str(result).encode("utf-8"))
-        f.write(b"\n")
-
-
-def iterate_slow(pipe, dataset, f):
-    for i, item in enumerate(tqdm.tqdm(dataset)):
-        try:
-            result = pipe(item)
-        except Exception as e:
-            result = {"error": str(e)}
-
-        f.write(str(result).encode("utf-8"))
-        f.write(b"\n")
-
-
-def main():
-    dset = datasets.load_dataset(DATASET_NAME, name=DATASET_CONFIG, split=DATASET_SPLIT)
-
-    pipe = get_pipeline()
-
-    f = io.BytesIO()
-    filename = f"data_{DATASET_CONFIG}_{DATASET_SPLIT}_{DATASET_COLUMN}.txt"
-    # TODO change to .iter(...) to get max performance on GPUs
-    print("Start batch")
-
-    if hasattr(pipe, "iter"):
-        iterate(pipe, dset[DATASET_COLUMN], f)
-    else:
-        iterate_slow(pipe, dset[DATASET_COLUMN], f)
-
-    f.seek(0)
-
-    api = HfApi()
-    repo_id = REPO_ID
-    try:
-        api.upload_file(TOKEN, f, filename, repo_id, repo_type="dataset")
-    except Exception:
-        pass
-
-
 if __name__ == "__main__":
-    main()
+    batch(
+        dataset_name=DATASET_NAME,
+        dataset_config=DATASET_CONFIG,
+        dataset_split=DATASET_SPLIT,
+        dataset_column=DATASET_COLUMN,
+        token=TOKEN,
+        repo_id=REPO_ID,
+        use_gpu=USE_GPU,
+        pipeline=get_pipeline(),
+    )
