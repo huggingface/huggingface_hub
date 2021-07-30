084fe67da80f01f8d928c6de5ebbc9ad3de592da
Nicolas Patry <patry.nicolas@protonmail.com>
2021-07-30 17:59:26 +0200

    Trying to make batching part of the lib for easier modifications. (#240)
    * Trying to make batching part of the lib for easier modifications.
    
    * Batching with a test.
    
    * Fixingstyle.
    
    * Unused import.
    
    * Fixing requirements.
    
    * Adding datasets requirments (for batch).
    
    * Fixing dependencies ?
    
    * Reduce requirements.
    
    * Fixing requirements + secrects.
    
    * Cleaner requirements

--- api-inference-community/setup.py
+++ api-inference-community/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="api_inference_community",
-    version="0.0.11",
+    version="0.0.13",
     description="A package with helper tools to build an API Inference docker app for Hugging Face API inference using huggingface_hub",
     url="http://github.com/huggingface/api-inference-community",
     author="Nicolas Patry",
@@ -17,7 +17,6 @@ setup(
         "test": [
             "httpx==0.18",
             "Pillow==8.2",
-            "huggingface_hub==0.0.13",
             "httpx==0.18",
             "torch==1.9.0",
             "pytest==6.2",
