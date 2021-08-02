bfc6a802b65073ed31b00f0bf1c43b27404f6dcb
Omar Sanseviero <osanseviero@gmail.com>
2021-08-02 11:25:52 +0200

    Add base generic inference and token classification example

--- api-inference-community/docker_images/generic/app/main.py
+++ api-inference-community/docker_images/generic/app/main.py
@@ -4,7 +4,7 @@ import os
 from typing import Dict, Type
 
 from api_inference_community.routes import pipeline_route, status_ok
-from app.pipelines import Pipeline
+from app.pipelines import Pipeline, TokenClassificationPipeline
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.gzip import GZipMiddleware
@@ -33,7 +33,7 @@ logger = logging.getLogger(__name__)
 # You can check the requirements and expectations of each pipelines in their respective
 # directories. Implement directly within the directories.
 ALLOWED_TASKS: Dict[str, Type[Pipeline]] = {
-    # IMPLEMENT_THIS: Add your implemented tasks here !
+    "token-classification": TokenClassificationPipeline
 }
 
 
