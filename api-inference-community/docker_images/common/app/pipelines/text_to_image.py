90d0e1c792a2d07d70fd9fbd090829bb2e5b1197
Omar Sanseviero <osanseviero@users.noreply.github.com>
2021-08-03 17:08:04 +0200

    Update api-inference-community/docker_images/common/app/pipelines/text_to_image.py
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- api-inference-community/docker_images/common/app/pipelines/text_to_image.py
+++ api-inference-community/docker_images/common/app/pipelines/text_to_image.py
@@ -4,7 +4,7 @@ from app.pipelines import Pipeline
 class TextToImagePipeline(Pipeline):
     def __init__(self, model_id: str):
         # IMPLEMENT_THIS
-        # Preload all the elements you are going to need at inference.
+        # Preload all the elements you are going to need for inference.
         # For instance your model, processors, tokenizer that might be needed.
         # This function is only called once, so do all the heavy processing I/O here
         raise NotImplementedError(
