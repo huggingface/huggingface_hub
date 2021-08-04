d897c40cbe3ec1df97db3dccef6b9d14f52be635
Omar Sanseviero <osanseviero@gmail.com>
2021-08-04 17:37:42 +0200

    Prettier

--- widgets/src/lib/Icons/IconStructuredDataClassification.svelte
+++ widgets/src/lib/Icons/IconStructuredDataClassification.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let classNames = "";
+
 </script>
 
 <svg
@@ -15,8 +16,8 @@
 	preserveAspectRatio="xMidYMid meet"
 	viewBox="0 0 32 32"
 >
-    <path 
-        d="M29 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v22a2 2 0 0 0 2 2h22a2 2 0 0 0 2-2zm-2 0v4H5V5zm0 22H5v-4h22zm0-6H5v-4h22zm0-6H5v-4h22z" 
-        fill="currentColor"
-    />
+	<path
+		d="M29 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v22a2 2 0 0 0 2 2h22a2 2 0 0 0 2-2zm-2 0v4H5V5zm0 22H5v-4h22zm0-6H5v-4h22zm0-6H5v-4h22z"
+		fill="currentColor"
+	/>
 </svg>
--- widgets/src/lib/Icons/IconTextToImage.svelte
+++ widgets/src/lib/Icons/IconTextToImage.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let classNames = "";
+
 </script>
 
 <svg
@@ -15,8 +16,11 @@
 	preserveAspectRatio="xMidYMid meet"
 	viewBox="0 0 32 32"
 >
-	<path 
-		d="M29.537 13.76l-3.297-3.297a1.586 1.586 0 0 0-2.24 0L10 24.467V30h5.533l14.004-14a1.586 1.586 0 0 0 0-2.24zM14.704 28H12v-2.704l9.44-9.441l2.705 2.704zM25.56 17.145l-2.704-2.704l2.267-2.267l2.704 2.704z" fill="currentColor"></path><path d="M11 17h2v-7h3V8H8v2h3v7z" fill="currentColor"></path><path d="M8 20H4V4h16v4h2V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h4z" 
+	<path
+		d="M29.537 13.76l-3.297-3.297a1.586 1.586 0 0 0-2.24 0L10 24.467V30h5.533l14.004-14a1.586 1.586 0 0 0 0-2.24zM14.704 28H12v-2.704l9.44-9.441l2.705 2.704zM25.56 17.145l-2.704-2.704l2.267-2.267l2.704 2.704z"
 		fill="currentColor"
-    />
+	/><path d="M11 17h2v-7h3V8H8v2h3v7z" fill="currentColor" /><path
+		d="M8 20H4V4h16v4h2V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h4z"
+		fill="currentColor"
+	/>
 </svg>
