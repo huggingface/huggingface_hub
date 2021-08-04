005134d8692e00d1371c07ddcad4603c04f18f64
devpramod <devpramod@github.org>
2021-08-04 11:24:28 -0400

    add text classification common template, change __call__ docstring

--- api-inference-community/docker_images/spacy/app/pipelines/text_classification.py
+++ api-inference-community/docker_images/spacy/app/pipelines/text_classification.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import sys
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from app.pipelines import Pipeline
 
@@ -28,18 +28,15 @@ class TextClassificationPipeline(Pipeline):
 
         self.model = spacy.load(model_name)
 
-    def __call__(self, inputs: str) -> List[Dict[str, Any]]:
+    def __call__(self, inputs: str) -> Dict[str, Any]:
         """
         Args:
             inputs (:obj:`str`):
                 a string containing some text
         Return:
-            A :obj:`list`:. The object returned should be like [{"entity_group": "XXX", "word": "some word", "start": 3, "end": 6, "score": 0.82}] containing :
-                - "entity_group": A string representing what the entity is.
-                - "word": A rubstring of the original string that was detected as an entity.
-                - "start": the offset within `input` leading to `answer`. context[start:stop] == word
-                - "end": the ending offset within `input` leading to `answer`. context[start:stop] === word
-                - "score": A score between 0 and 1 describing how confident the model is for this entity.
+            A :obj:`dict`:. The object returned should be like {"label": 0.9939950108528137} containing :
+                - "label": A string representing what the label/class is. There can be multiple labels.
+                - "score": A score between 0 and 1 describing how confident the model is for this label/class.
         """
         doc = self.model(inputs)
 
