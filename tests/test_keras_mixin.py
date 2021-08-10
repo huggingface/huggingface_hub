fc83365aafc36d65e3db9acf01c96008f42181b6
Nathan Raw <nxr9266@g.rit.edu>
2021-08-10 18:35:21 -0400

    :bug: PT -> TF in require_tf docstring
    Co-authored-by: Omar Sanseviero <osanseviero@users.noreply.github.com>

--- tests/test_keras_mixin.py
+++ tests/test_keras_mixin.py
@@ -24,9 +24,9 @@ if is_tf_available():
 
 def require_tf(test_case):
     """
-    Decorator marking a test that requires PyTorch.
+    Decorator marking a test that requires TensorFlow.
 
-    These tests are skipped when PyTorch isn't installed.
+    These tests are skipped when TensorFlow isn't installed.
 
     """
     if not is_tf_available():
