6926fd9bec23cb963ce3f58ec53496083997f0fa
devpramod <60084015+devpramod@users.noreply.github.com>
2021-08-11 06:11:58 -0400

    Add text classification for Spacy  (#232)
    * first commit; added empty script
    
    * text classification pipeline for spacy
    
    * add text classification common template, change __call__ docstring
    
    * changed output type, added new tests
    
    * changed test case condition
    
    Co-authored-by: devpramod <devpramod@github.org>

--- api-inference-community/docker_images/spacy/app/pipelines/__init__.py
+++ api-inference-community/docker_images/spacy/app/pipelines/__init__.py
@@ -8,5 +8,6 @@ from app.pipelines.feature_extraction import FeatureExtractionPipeline
 from app.pipelines.image_classification import ImageClassificationPipeline
 from app.pipelines.question_answering import QuestionAnsweringPipeline
 from app.pipelines.sentence_similarity import SentenceSimilarityPipeline
+from app.pipelines.text_classification import TextClassificationPipeline
 from app.pipelines.text_to_speech import TextToSpeechPipeline
 from app.pipelines.token_classification import TokenClassificationPipeline
