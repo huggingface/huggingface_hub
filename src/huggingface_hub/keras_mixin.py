a09643a1d1f980d4b91b45ee87af19229e8888d1
nateraw <nxr9266@g.rit.edu>
2021-08-16 19:48:20 -0400

    :construction: wip open heart surgery on keras mixin/utils

--- src/huggingface_hub/keras_mixin.py
+++ src/huggingface_hub/keras_mixin.py
@@ -2,26 +2,47 @@ import logging
 import os
 from pathlib import Path
 
+from huggingface_hub.file_download import is_tf_available
+from huggingface_hub.snapshot_download import snapshot_download
 from huggingface_hub import ModelHubMixin, hf_hub_download
+from huggingface_hub.constants import HUGGINGFACE_HUB_CACHE
 
 
 logger = logging.getLogger(__name__)
 
+if is_tf_available():
+    import tensorflow as tf
+
+
+def save_pretrained_keras(model, save_dir):
+    """Function mimicing save_pretrained. Use this if you're using the Functional or Sequential APIs."""
+    if not model.built:
+        raise ValueError("Model should be built before trying to save")
+
+    tf.keras.models.save_model(model, save_dir)
+
+def from_pretrained_keras(model_name_or_path, revision=None, cache_dir=None, **kwargs):
+    """Function mimicing from_pretrained. Use this if you're using the Functional or Sequential APIs."""    
+
+    # Root is either a local filepath matching model_id or a cached snapshot
+    storage_folder = snapshot_download(
+        repo_id=model_name_or_path,
+        revision=revision,
+        cache_dir=cache_dir
+    ) if not os.path.isdir(model_name_or_path) else model_name_or_path
+
+    return tf.keras.models.load_model(storage_folder, **kwargs)
+
+def push_to_hub_keras(model, *args, **kwargs):
+    """Here we simply call the mixin's push_to_hub function. Would it be better to do it the other way around?"""
+    return ModelHubMixin.push_to_hub(model, *args, **kwargs)
+
 
 class KerasModelHubMixin(ModelHubMixin):
-
-    _CONFIG_NAME = "config.json"
-    _WEIGHTS_NAME = "tf_model.h5"
 
     def __init__(self, *args, **kwargs):
         """
         Mix this class with your keras-model class for ease process of saving & loading from huggingface-hub
-
-        NOTE - Dummy Inputs are required to save/load models using this mixin. When saving, you are required to either:
-
-            1. Assign an attribute to your class, self.dummy_inputs, that defines inputs to be passed to the model's call
-            function to build the model.
-            2. Pass the dummy_inputs kwarg to save_pretrained. We will save this along with the model (as if it were an attribute).
 
         Example::
 
@@ -35,34 +56,23 @@ class KerasModelHubMixin(ModelHubMixin):
             ...        self.layer = ...
             ...    def call(self, ...)
             ...        return ...
-
+            
+            >>> # Init and compile the model as you normally would
             >>> model = MyModel()
-            >>> model.save_pretrained("mymodel", push_to_hub=False) # Saving model weights in the directory
-            >>> model.push_to_hub("mymodel", "model-1") # Pushing model-weights to hf-hub
+            >>> model.compile(...)
+            >>> # Build the graph by training it or passing dummy inputs
+            >>> model(model.dummy_inputs)
+            >>> # You can save your model like this
+            >>> model.save_pretrained("local_model_dir/", push_to_hub=False)
+            >>> # Or, you can push to a new public model repo like this
+            >>> model.push_to_hub("super-cool-model", git_user="your-hf-username", git_email="you@somesite.com")
 
             >>> # Downloading weights from hf-hub & model will be initialized from those weights
             >>> model = MyModel.from_pretrained("username/mymodel@main")
         """
 
-    def _save_pretrained(self, save_directory, dummy_inputs=None, **kwargs):
-
-        dummy_inputs = (
-            dummy_inputs
-            if dummy_inputs is not None
-            else getattr(self, "dummy_inputs", None)
-        )
-
-        if dummy_inputs is None:
-            raise RuntimeError(
-                "You must either provide dummy inputs or have them assigned as an attribute of this model"
-            )
-
-        _ = self(dummy_inputs, training=False)
-
-        save_directory = Path(save_directory)
-        model_file = save_directory / self._WEIGHTS_NAME
-        self.save_weights(model_file)
-        logger.info(f"Model weights saved in {model_file}")
+    def _save_pretrained(self, save_directory):
+        save_pretrained_keras(self, save_directory)
 
     @classmethod
     def _from_pretrained(
@@ -75,34 +85,10 @@ class KerasModelHubMixin(ModelHubMixin):
         resume_download,
         local_files_only,
         use_auth_token,
-        by_name=False,
         **model_kwargs,
     ):
-        if os.path.isdir(model_id):
-            print("Loading weights from local directory")
-            model_file = os.path.join(model_id, cls._WEIGHTS_NAME)
-        else:
-            model_file = hf_hub_download(
-                repo_id=model_id,
-                filename=cls._WEIGHTS_NAME,
-                revision=revision,
-                cache_dir=cache_dir,
-                force_download=force_download,
-                proxies=proxies,
-                resume_download=resume_download,
-                use_auth_token=use_auth_token,
-                local_files_only=local_files_only,
-            )
-
-        model = cls(**model_kwargs)
-
-        if hasattr(model, "dummy_inputs") and model.dummy_inputs is not None:
-            raise ValueError("Model must have a dummy_inputs attribute")
-
-        _ = model(model.dummy_inputs, training=False)
-
-        model.load_weights(model_file, by_name=by_name)
-
-        _ = model(model.dummy_inputs, training=False)
-
-        return model
+        """Here we just call from_pretrained_keras function so both the mixin and functional APIs stay in sync.
+        
+        TODO - Some args above aren't used since we are calling snapshot_download instead of hf_hub_download.
+        """
+        return from_pretrained_keras(model_id, revision, cache_dir, **model_kwargs)
