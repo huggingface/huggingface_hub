43d514bc7631d5d5ed3149f408a1cd629390fee3
Mishig Davaadorj <dmishig@gmail.com>
2021-08-16 11:35:48 +0200

    Implement text-to-image widget

--- widgets/src/lib/InferenceWidget/InferenceWidget.svelte
+++ widgets/src/lib/InferenceWidget/InferenceWidget.svelte
@@ -14,6 +14,7 @@
 	import SummarizationWidget from "./widgets/SummarizationWidget/SummarizationWidget.svelte";
 	import TableQuestionAnsweringWidget from "./widgets/TableQuestionAnsweringWidget/TableQuestionAnsweringWidget.svelte";
 	import TextGenerationWidget from "./widgets/TextGenerationWidget/TextGenerationWidget.svelte";
+	import TextToImageWidget from "./widgets/TextToImageWidget/TextToImageWidget.svelte";
 	import TextToSpeechWidget from "./widgets/TextToSpeechWidget/TextToSpeechWidget.svelte";
 	import TokenClassificationWidget from "./widgets/TokenClassificationWidget/TokenClassificationWidget.svelte";
 	import StructuredDataClassificationWidget from "./widgets/StructuredDataClassificationWidget/StructuredDataClassificationWidget.svelte";
@@ -48,6 +49,7 @@
 		"text-classification": FillMaskWidget,
 		"text-generation": TextGenerationWidget,
 		"token-classification": TokenClassificationWidget,
+		"text-to-image": TextToImageWidget,
 		"text-to-speech": TextToSpeechWidget,
 		translation: TextGenerationWidget,
 		"structured-data-classification": StructuredDataClassificationWidget,
