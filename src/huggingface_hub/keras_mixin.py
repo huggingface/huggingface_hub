535b1d789e6a32a1bc494ff132d6305c3153cd79
nateraw <nxr9266@g.rit.edu>
2021-08-16 20:31:40 -0400

    :construction: wip

--- src/huggingface_hub/keras_mixin.py
+++ src/huggingface_hub/keras_mixin.py
@@ -1,11 +1,14 @@
 import logging
 import os
 from pathlib import Path
+from typing import Optional, Union
 
 from huggingface_hub.file_download import is_tf_available
 from huggingface_hub.snapshot_download import snapshot_download
 from huggingface_hub import ModelHubMixin, hf_hub_download
 from huggingface_hub.constants import HUGGINGFACE_HUB_CACHE
+from .hf_api import HfApi, HfFolder
+from .repository import Repository
 
 
 logger = logging.getLogger(__name__)
@@ -21,6 +24,7 @@ def save_pretrained_keras(model, save_dir):
 
     tf.keras.models.save_model(model, save_dir)
 
+
 def from_pretrained_keras(model_name_or_path, revision=None, cache_dir=None, **kwargs):
     """Function mimicing from_pretrained. Use this if you're using the Functional or Sequential APIs."""    
 
@@ -33,9 +37,68 @@ def from_pretrained_keras(model_name_or_path, revision=None, cache_dir=None, **k
 
     return tf.keras.models.load_model(storage_folder, **kwargs)
 
-def push_to_hub_keras(model, *args, **kwargs):
-    """Here we simply call the mixin's push_to_hub function. Would it be better to do it the other way around?"""
-    return ModelHubMixin.push_to_hub(model, *args, **kwargs)
+
+def push_to_hub_keras(
+    model,
+    repo_path_or_name: Optional[str] = None,
+    repo_url: Optional[str] = None,
+    commit_message: Optional[str] = "Add model",
+    organization: Optional[str] = None,
+    private: Optional[bool] = None,
+    api_endpoint: Optional[str] = None,
+    use_auth_token: Optional[Union[bool, str]] = None,
+    git_user: Optional[str] = None,
+    git_email: Optional[str] = None,
+    config: Optional[dict] = None,
+):
+    if repo_path_or_name is None and repo_url is None:
+        raise ValueError(
+            "You need to specify a `repo_path_or_name` or a `repo_url`."
+        )
+
+    if use_auth_token is None and repo_url is None:
+        token = HfFolder.get_token()
+        if token is None:
+            raise ValueError(
+                "You must login to the Hugging Face hub on this computer by typing `transformers-cli login` and "
+                "entering your credentials to use `use_auth_token=True`. Alternatively, you can pass your own "
+                "token as the `use_auth_token` argument."
+            )
+    elif isinstance(use_auth_token, str):
+        token = use_auth_token
+    else:
+        token = None
+
+    if repo_path_or_name is None:
+        repo_path_or_name = repo_url.split("/")[-1]
+
+    # If no URL is passed and there's no path to a directory containing files, create a repo
+    if repo_url is None and not os.path.exists(repo_path_or_name):
+        repo_name = Path(repo_path_or_name).name
+        repo_url = HfApi(endpoint=api_endpoint).create_repo(
+            token,
+            repo_name,
+            organization=organization,
+            private=private,
+            repo_type=None,
+            exist_ok=True,
+        )
+
+    repo = Repository(
+        repo_path_or_name,
+        clone_from=repo_url,
+        use_auth_token=use_auth_token,
+        git_user=git_user,
+        git_email=git_email,
+    )
+    repo.git_pull(rebase=True)
+
+    save_pretrained_keras(model, repo_path_or_name, config=config)
+
+    # Commit and push!
+    repo.git_add()
+    repo.git_commit(commit_message)
+    return repo.git_push()
 
 
 class KerasModelHubMixin(ModelHubMixin):
