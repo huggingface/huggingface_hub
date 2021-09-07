61112954fbe738a65d8069103064e82f238fef3e
Omar Sanseviero <osanseviero@gmail.com>
2021-09-07 21:24:43 +0200

    Quality

--- api-inference-community/docker_images/espnet/app/pipelines/__init__.py
+++ api-inference-community/docker_images/espnet/app/pipelines/__init__.py
@@ -1,4 +1,6 @@
 from app.pipelines.base import Pipeline, PipelineException  # isort:skip
 
-from app.pipelines.automatic_speech_recognition import AutomaticSpeechRecognitionPipeline
+from app.pipelines.automatic_speech_recognition import (
+    AutomaticSpeechRecognitionPipeline,
+)
 from app.pipelines.text_to_speech import TextToSpeechPipeline
