000b9eeafe8730b03ae7a7152e12f255b1e25ca5
Mishig Davaadorj <dmishig@gmail.com>
2021-09-08 17:22:35 +0200

    Module exorts

--- widgets/src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as InferenceWidget } from './InferenceWidget/InferenceWidget.svelte';
-export { default as ModelPipelineIcon } from './ModelPipelineIcon/ModelPipelineIcon.svelte';
-export { default as ModelPipelineTag } from './ModelPipelineTag/ModelPipelineTag.svelte';
--- widgets/src/lib/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export * as DefaultWidget from './DefaultWidget';
-export * as Language from './Language';
-export * as Libraries from './Libraries';
-export * as Types from './Types';
