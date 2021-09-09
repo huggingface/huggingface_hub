5b8492e04220467f847bb60ed03de8c3411fc0d3
Mishig Davaadorj <dmishig@gmail.com>
2021-09-09 10:44:54 +0200

    Export docs

--- widgets/postpackage.js
+++ widgets/postpackage.js
@@ -1,7 +1,25 @@
 import fs from "fs";
+import path from "path";
 
-const path = './package/package.json';
-let pkg = fs.readFileSync(path);
+const copyRecursiveSync = (src, dest) => {
+    const exists = fs.existsSync(src);
+    const stats = exists && fs.statSync(src);
+    const isDirectory = exists && stats.isDirectory();
+    if (isDirectory) {
+      fs.mkdirSync(dest);
+      fs.readdirSync(src).forEach(function(childItemName) {
+        copyRecursiveSync(path.join(src, childItemName),
+                          path.join(dest, childItemName));
+      });
+    } else {
+      fs.copyFileSync(src, dest);
+    }
+  };
+
+const pathtToJson = './package/package.json';
+let pkg = fs.readFileSync(pathtToJson);
 pkg = JSON.parse(pkg);
 delete pkg.type;
-fs.writeFileSync(path, JSON.stringify(pkg));
+fs.writeFileSync(pathtToJson, JSON.stringify(pkg));
+
+copyRecursiveSync("../docs", "./package/docs");
