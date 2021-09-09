c95e48ea5adf2a75077665471e8dc499474eda49
Mishig Davaadorj <dmishig@gmail.com>
2021-09-09 15:48:40 +0200

    Correct scaling

--- widgets/src/lib/InferenceWidget/widgets/ObjectDetectionWidget/SvgBoundingBoxes.svelte
+++ widgets/src/lib/InferenceWidget/widgets/ObjectDetectionWidget/SvgBoundingBoxes.svelte
@@ -1,27 +1,41 @@
 <script>
 	import type { Box } from "../../shared/types";
-	// include .js ext so that `svelte-kit build` can find the file
+	// include .js ext so that `svelte-kit build` can find the module
 	import * as colors from "tailwindcss/colors.js";
-	let height = 0;
-	let width = 0;
+
+	let imgEl: HTMLImageElement;
+	let wrapperHeight = 0;
+	let wrapperWidth = 0;
+	let boxes: Array<{
+		color: string;
+		index: number;
+		rect: { x: number; y: number; width: number; height: number };
+	}> = [];
+
 	export let imgSrc = "";
 	export let highlightIndex = -1;
 	export let output: Array<{ box: Box; color: string }> = [];
 	export let mouseover: (index: number) => void = () => {};
 	export let mouseout: () => void = () => {};
 
-	$: boxes = output
-		.map((val, index) => ({ ...val, index }))
-		.sort((a, b) => getArea(b.box) - getArea(a.box))
-		.map(({ box, color, index }) => {
-			const rect = {
-				x: box.xmin,
-				y: box.ymin,
-				width: box.xmax - box.xmin,
-				height: box.ymax - box.ymin,
-			};
-			return { rect, color, index };
-		});
+	$: {
+		if (imgEl?.naturalWidth && imgEl?.naturalHeight) {
+			const widthScale = wrapperWidth / imgEl.naturalWidth;
+			const heightScale = wrapperHeight / imgEl.naturalHeight;
+			boxes = output
+				.map((val, index) => ({ ...val, index }))
+				.sort((a, b) => getArea(b.box) - getArea(a.box))
+				.map(({ box, color, index }) => {
+					const rect = {
+						x: box.xmin * widthScale,
+						y: box.ymin * heightScale,
+						width: (box.xmax - box.xmin) * widthScale,
+						height: (box.ymax - box.ymin) * heightScale,
+					};
+					return { rect, color, index };
+				});
+		}
+	}
 
 	function getArea(box: Box): number {
 		return (box.xmax - box.xmin) * (box.ymax - box.ymin);
@@ -31,14 +45,14 @@
 {#if imgSrc}
 	<div
 		class="relative top-0 left-0 inline-flex"
-		bind:clientWidth={width}
-		bind:clientHeight={height}
+		bind:clientWidth={wrapperWidth}
+		bind:clientHeight={wrapperHeight}
 	>
-		<img alt="" class="relative top-0 left-0" src={imgSrc} />
+		<img alt="" class="relative top-0 left-0" src={imgSrc} bind:this={imgEl} />
 
 		<svg
 			class="absolute top-0 left-0"
-			viewBox={`0 0 ${width} ${height}`}
+			viewBox={`0 0 ${wrapperWidth} ${wrapperHeight}`}
 			xmlns="http://www.w3.org/2000/svg"
 		>
 			{#each boxes as { rect, color, index }}
