0918ccd9a5661901c598164cb82bce8a58329fd7
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-10-04 18:08:12 -0400

    Adapt test

--- tests/test_snapshot_download.py
+++ tests/test_snapshot_download.py
@@ -92,7 +92,7 @@ class SnapshotDownloadTests(unittest.TestCase):
             self.assertTrue(self.first_commit_hash in storage_folder)
 
     def test_download_private_model(self):
-        self._api.update_repo_visibility(self._token, REPO_NAME, private=True)
+        self._api.update_repo_visibility(token=self._token, name=REPO_NAME, private=True)
 
         # Test download fails without token
         with tempfile.TemporaryDirectory() as tmpdirname:
