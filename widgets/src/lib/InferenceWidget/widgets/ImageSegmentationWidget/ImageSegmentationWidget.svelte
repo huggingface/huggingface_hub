2f4ea5cd6227ae84d38e62248403ca8c5d4804c7
Mishig Davaadorj <dmishig@gmail.com>
2021-11-01 16:13:14 +0100

    Rm unnecessary parts of polufill

--- widgets/src/lib/InferenceWidget/widgets/ImageSegmentationWidget/ImageSegmentationWidget.svelte
+++ widgets/src/lib/InferenceWidget/widgets/ImageSegmentationWidget/ImageSegmentationWidget.svelte
@@ -190,24 +190,15 @@
 	// original: https://gist.github.com/MonsieurV/fb640c29084c171b4444184858a91bc7
 	function polyfillCreateImageBitmap() {
 		window.createImageBitmap = async function (
-			data: ImageBitmapSource
+			data: ImageData
 		): Promise<ImageBitmap> {
 			return new Promise((resolve, reject) => {
-				let dataURL: string;
-				if (data instanceof Blob) {
-					dataURL = URL.createObjectURL(data);
-				} else if (data instanceof ImageData) {
-					const canvas = document.createElement("canvas");
-					const ctx = canvas.getContext("2d");
-					canvas.width = data.width;
-					canvas.height = data.height;
-					ctx.putImageData(data, 0, 0);
-					dataURL = canvas.toDataURL();
-				} else {
-					reject(
-						"createImageBitmap does not handle the provided image source type"
-					);
-				}
+				const canvas = document.createElement("canvas");
+				const ctx = canvas.getContext("2d");
+				canvas.width = data.width;
+				canvas.height = data.height;
+				ctx.putImageData(data, 0, 0);
+				const dataURL = canvas.toDataURL();
 				const img = document.createElement("img");
 				img.addEventListener("load", () => {
 					resolve(img as any as ImageBitmap);
