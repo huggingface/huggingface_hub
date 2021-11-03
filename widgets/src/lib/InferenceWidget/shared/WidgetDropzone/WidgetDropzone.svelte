ca7595683c4d7e957cc7279be1088a4372ffa1eb
Mishig Davaadorj <dmishig@gmail.com>
2021-11-03 15:24:10 +0100

    Fix dropzone pointer-events-none

--- widgets/src/lib/InferenceWidget/shared/WidgetDropzone/WidgetDropzone.svelte
+++ widgets/src/lib/InferenceWidget/shared/WidgetDropzone/WidgetDropzone.svelte
@@ -79,7 +79,7 @@
 	{#if !imgSrc}
 		<span class="pointer-events-none text-sm">{label}</span>
 	{:else}
-		<div class="pointer-events-none">
+		<div class={isDragging && "pointer-events-none"}>
 			<slot />
 		</div>
 	{/if}
