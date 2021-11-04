7f334551d0cc4678247602b594c8d7220e79ef14
Mishig Davaadorj <mishig.davaadorj@coloradocollege.edu>
2021-11-04 18:04:32 +0100

    Lint (#457)

--- widgets/src/lib/InferenceWidget/shared/ViewUtils.ts
+++ widgets/src/lib/InferenceWidget/shared/ViewUtils.ts
@@ -151,7 +151,7 @@ export function hexToRgb(hex: string): number[]{
 	return hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
 			   ,(_, r, g, b) => '#' + r + r + g + g + b + b)
 	  .substring(1).match(/.{2}/g)
-	  .map(x => parseInt(x, 16))
+	  ?.map(x => parseInt(x, 16)) || [0, 0, 0];
 }
 
 /*
