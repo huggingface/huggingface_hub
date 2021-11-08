89869a8c4120313092d3bc6a9461ac5d0a95267c
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2021-11-08 14:01:42 -0500

    Dev version

--- src/huggingface_hub/__init__.py
+++ src/huggingface_hub/__init__.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.1.2"
+__version__ = "0.1.3.dev0"
 
 from .commands.user import notebook_login
 from .constants import (
