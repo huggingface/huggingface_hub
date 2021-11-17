bd726dde9cd0739f0f1abc4b639ff6715c2b4dad
Victor Muštar <victor.mustar@gmail.com>
2021-11-17 09:43:21 +0100

    padding contenteditable

--- widgets/src/lib/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
+++ widgets/src/lib/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
@@ -12,7 +12,7 @@
 			bind:textContent={value}
 			class="{label
 				? 'mt-1.5'
-				: ''} block overflow-auto resize-y py-1 px-2 w-full max-h-[500px] border border-gray-200 rounded-lg shadow-inner outline-none focus:ring-1 focus:ring-inset focus:ring-indigo-200 focus:shadow-inner dark:bg-gray-925"
+				: ''} block overflow-auto resize-y py-2 px-3 w-full max-h-[500px] border border-gray-200 rounded-lg shadow-inner outline-none focus:ring-1 focus:ring-inset focus:ring-indigo-200 focus:shadow-inner dark:bg-gray-925"
 			role="textbox"
 			contenteditable
 			style="--placeholder: '{placeholder}'"
