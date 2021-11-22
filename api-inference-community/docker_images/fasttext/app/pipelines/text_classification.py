6117e508cd39b2e264adc054639be7d2662b29a2
Omar Sanseviero <osanseviero@gmail.com>
2021-11-22 15:54:04 +0100

    Reduce k in text classification

--- api-inference-community/docker_images/fasttext/app/pipelines/text_classification.py
+++ api-inference-community/docker_images/fasttext/app/pipelines/text_classification.py
@@ -22,7 +22,7 @@ class TextClassificationPipeline(Pipeline):
         """
         if len(inputs.split()) > 1:
             raise ValueError("Expected input is a single word")
-        preds = self.model.get_nearest_neighbors(inputs, k=10)
+        preds = self.model.get_nearest_neighbors(inputs, k=5)
         result = []
         for distance, word in preds:
             result.append({"label": word, "score": distance})
