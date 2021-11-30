b3470b5ae821148b3af3d14bf9f128a788d44c2b
Patrick von Platen <patrick.v.platen@gmail.com>
2021-11-30 18:17:08 +0100

    improve snapshot download

--- src/huggingface_hub/snapshot_download.py
+++ src/huggingface_hub/snapshot_download.py
@@ -1,15 +1,19 @@
-import glob
 import os
+from glob import glob
 from pathlib import Path
 from typing import Dict, Optional, Union
 
 from .constants import DEFAULT_REVISION, HUGGINGFACE_HUB_CACHE
 from .file_download import cached_download, hf_hub_url
 from .hf_api import HfApi, HfFolder
+from .utils import logging
 
 
 REPO_ID_SEPARATOR = "__"
 # ^ make sure this substring is not allowed in repo_ids on hf.co
+
+
+logger = logging.get_logger(__name__)
 
 
 def snapshot_download(
@@ -66,33 +70,73 @@ def snapshot_download(
     # if we have no internet connection we will look for the
     # last modified folder in the cache
     if local_files_only:
-        # list all possible folders that correspond to the repo_id
-        repo_id_folders = glob.glob(
-            os.path.join(cache_dir, repo_id_flattened + "." + revision + ".*")
+        # possible repos have <path/to/cache_dir>/<flatten_repo_id> prefix
+        repo_folders_prefix = os.path.join(cache_dir, repo_id_flattened)
+
+        # list all possible folders that can correspond to the repo_id
+        # and are of the format <flattened-repo-id>.<revision>.<commit-sha>
+        # now let's list all cached repos that have to be included in the revision
+        # 1) cached repos of format <repo_id>.{revision}.<any-hash>
+        repo_folders_branch = glob(repo_folders_prefix + "." + revision + ".*")
+
+        # 2) cached repos of format <repo_id>.{revision}
+        # -> in this case {revision} has to be a commit sha
+        repo_folders_commit_only = glob(repo_folders_prefix + "." + revision)
+
+        # 3) cached repos of format <repo_id>.<any-branch>.{revision}
+        # -> in this case {revision} also has to be a commit sha
+        repo_folders_branch_commit = glob(repo_folders_prefix + ".*." + revision)
+
+        # combine all possible fetched cached repos
+        repo_folders = (
+            repo_folders_branch + repo_folders_commit_only + repo_folders_branch_commit
         )
 
-        if len(repo_id_folders) == 0:
+        if len(repo_folders) == 0:
             raise ValueError(
                 "Cannot find the requested files in the cached path and outgoing traffic has been"
                 " disabled. To enable model look-ups and downloads online, set 'local_files_only'"
                 " to False."
             )
 
-        # find last modified folder
-        storage_folder = max(
-            repo_id_folders,
-            key=os.path.getmtime,
+        # check if repo id was previously cached from a commit sha revision
+        # and passed {revision} is not a commit sha
+        # in this case snapshotting repos locally might lead to unexpected
+        # behavior the user should be warned about
+        repo_folders_commit_any = set(glob(repo_folders_prefix + ".*")) - set(
+            glob(repo_folders_prefix + ".*.*")
         )
+        if (
+            len(repo_folders_commit_any) > 0
+            and len(repo_folders_commit_only + repo_folders_branch_commit) == 0
+        ):
+            logger.warn(
+                f"The repo {repo_id} was previously downloaded from a commit hash revision "
+                f"and has created the following cached directories {repo_folders_commit_any}."
+                f" In this case, trying to load a repo from the branch {revision} in offline "
+                "mode might lead to unexpected behavior by not taking into account the latest "
+                "commits."
+            )
 
+        # find last modified folder
+        storage_folder = max(repo_folders, key=os.path.getmtime)
+
+        # get commit sha
         repo_id_sha = storage_folder.split(".")[-1]
         model_files = os.listdir(storage_folder)
     else:
         # if we have internet connection we retrieve the correct folder name from the huggingface api
         _api = HfApi()
         model_info = _api.model_info(repo_id=repo_id, revision=revision, token=token)
-        storage_folder = os.path.join(
-            cache_dir, repo_id_flattened + "." + revision + "." + model_info.sha
-        )
+
+        storage_folder = os.path.join(cache_dir, repo_id_flattened + "." + revision)
+
+        # if passed revision is not identical to the commit sha
+        # then revision has to be a branch name, e.g. "main"
+        # in this case make sure that the branch name is included
+        # cached storage folder name
+        if revision != model_info.sha:
+            storage_folder += f".{model_info.sha}"
 
         repo_id_sha = model_info.sha
         model_files = [f.rfilename for f in model_info.siblings]
