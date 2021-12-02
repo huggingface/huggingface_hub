3c0aeec911f4940836f05419fb4bdbc8697983e5
Merve Noyan <merveenoyan@gmail.com>
2021-12-02 13:44:29 +0300

    Update tasks/src/fill-mask/data.ts
    Co-authored-by: Omar Sanseviero <osanseviero@users.noreply.github.com>

--- tasks/src/fill-mask/data.ts
+++ tasks/src/fill-mask/data.ts
@@ -55,7 +55,7 @@ const taskData: TaskData = {
 			id:          "xlm-roberta-base",
 		}
 	],
-	summary:      "Masked language modeling is the task of predicting which words should fill in the blanks of a sentence. The task doesn’t require labelled data, it’s trained by masking a couple of words in the sentences and model is expected to guess the masked word.",
+	summary:      "Masked language modeling is the task of predicting which words should fill in the blanks of a sentence. The task doesn’t require labelled data: it’s trained by masking words in the sentences, and the model is expected to predict the masked word.",
 	widgetModels: ["bert-base-uncased"],
 };
 
