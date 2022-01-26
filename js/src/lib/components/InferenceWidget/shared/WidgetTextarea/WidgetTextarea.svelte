ff30cc58e7138b40f3a52c32a054d98d5936fa6f
Mishig Davaadorj <dmishig@gmail.com>
2022-01-26 12:10:19 +0100

    Refactor

--- js/src/lib/components/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
+++ js/src/lib/components/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
@@ -1,35 +1,26 @@
 <script>
-	import { onMount } from "svelte";
-
 	import WidgetLabel from "../WidgetLabel/WidgetLabel.svelte";
 
 	export let label: string = "";
 	export let placeholder: string = "Your sentence here...";
 	export let value: string;
 
-	let spanEl: HTMLSpanElement;
+	function handlePaste(e: ClipboardEvent){
+		e.preventDefault();
 
-	function insertTextAtCaret(text: string) {
+		const text = e.clipboardData.getData("text/plain");
 		const sel = window.getSelection();
 		const range = sel.getRangeAt(0);
 		range.insertNode(document.createTextNode(text));
 		range.collapse(false);
 	}
-
-	onMount(() => {
-		spanEl.addEventListener("paste", (e) => {
-			e.preventDefault();
-			const text = e.clipboardData.getData("text/plain");
-			insertTextAtCaret(text);
-		});
-	});
 </script>
 
 <WidgetLabel {label}>
 	<svelte:fragment slot="after">
 		<span
 			bind:textContent={value}
-			bind:this={spanEl}
+			on:paste={handlePaste}
 			class="{label
 				? 'mt-1.5'
 				: ''} block overflow-auto resize-y py-2 px-3 w-full min-h-[42px] max-h-[500px] border border-gray-200 rounded-lg shadow-inner outline-none focus:ring-1 focus:ring-inset focus:ring-indigo-200 focus:shadow-inner dark:bg-gray-925"
