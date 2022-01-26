9b34fece7caee5897cf0074f1af326d44c227bb0
Mishig Davaadorj <dmishig@gmail.com>
2022-01-26 16:49:47 +0100

    Use `textarea` el

--- js/src/lib/components/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
+++ js/src/lib/components/InferenceWidget/shared/WidgetTextarea/WidgetTextarea.svelte
@@ -1,39 +1,40 @@
 <script>
+	import { onMount, tick } from "svelte";
+
 	import WidgetLabel from "../WidgetLabel/WidgetLabel.svelte";
 
 	export let label: string = "";
 	export let placeholder: string = "Your sentence here...";
 	export let value: string;
 
-	function handlePaste(e: ClipboardEvent) {
-		e.preventDefault();
+	let textAreaEl: HTMLTextAreaElement;
 
-		const text = e.clipboardData.getData("text/plain");
-		const sel = window.getSelection();
-		const range = sel.getRangeAt(0);
-		range.insertNode(document.createTextNode(text));
-		range.collapse(false);
+	const HEIGHT_LIMIT = 500 as const;
+
+	async function resize() {
+		if (!!textAreaEl) {
+			textAreaEl.style.height = "";
+			await tick();
+			textAreaEl.style.height =
+				Math.min(textAreaEl.scrollHeight, HEIGHT_LIMIT) + "px";
+		}
+	}
+
+	$: {
+		value;
+		resize();
 	}
 </script>
 
 <WidgetLabel {label}>
 	<svelte:fragment slot="after">
-		<span
-			bind:textContent={value}
-			on:paste={handlePaste}
+		<textarea
+			bind:this={textAreaEl}
+			bind:value
 			class="{label
 				? 'mt-1.5'
-				: ''} block overflow-auto resize-y py-2 px-3 w-full min-h-[42px] max-h-[500px] border border-gray-200 rounded-lg shadow-inner outline-none focus:ring-1 focus:ring-inset focus:ring-indigo-200 focus:shadow-inner dark:bg-gray-925"
-			role="textbox"
-			contenteditable
-			style="--placeholder: '{placeholder}'"
+				: ''} block w-full border border-gray-200 rounded-lg shadow-inner outline-none focus:ring-1 focus:ring-inset focus:ring-indigo-200 focus:shadow-inner dark:bg-gray-925"
+			{placeholder}
 		/>
 	</svelte:fragment>
 </WidgetLabel>
-
-<style>
-	span[contenteditable]:empty::before {
-		content: var(--placeholder);
-		color: rgba(156, 163, 175);
-	}
-</style>
