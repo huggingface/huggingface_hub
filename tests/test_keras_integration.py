6c3c6a753d4f690844adda7f2a0e7f0425213f86
Merve Noyan <merveenoyan@gmail.com>
2022-02-04 19:01:57 +0300

    Update tests/test_keras_integration.py
    Co-authored-by: Omar Sanseviero <osanseviero@gmail.com>

--- tests/test_keras_integration.py
+++ tests/test_keras_integration.py
@@ -311,8 +311,7 @@ class HubKerasSequentialTest(HubMixingTestKeras):
         model_info = HfApi(endpoint=ENDPOINT_STAGING).model_info(
             f"{USER}/{REPO_NAME}",
         )
-        model_files = [f.rfilename for f in model_info.siblings]
-        self.assertTrue("logs/tensorboard.txt" in model_files)
+        self.assertTrue("logs/tensorboard.txt" in model_info.siblings)
 
         self._api.delete_repo(name=f"{REPO_NAME}", token=self._token)
 
