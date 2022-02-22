2e36429f9446b036011a81a2e545bcb66f5b6874
Julien Simon <julien@julien.org>
2022-02-22 22:43:02 +0100

    Add support for private hub (#703)
    * Add support for private hub
    
    * Fix style
    
    * Make expression more pythonic
    
    Co-authored-by: Julien Simon <julsimon@huggingface.co>

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -410,7 +410,9 @@ def erase_from_credential_store(username=None):
 
 class HfApi:
     def __init__(self, endpoint=None):
-        self.endpoint = endpoint if endpoint is not None else ENDPOINT
+        self.endpoint = (
+            endpoint if endpoint is not None else os.getenv("HF_ENDPOINT", ENDPOINT)
+        )
 
     def login(self, username: str, password: str) -> str:
         """
