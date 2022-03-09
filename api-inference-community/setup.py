efe9471fe8722cc7cb8f44ef9b9f0d35615f15cf
Nicolas Patry <patry.nicolas@protonmail.com>
2022-03-09 16:12:17 +0100

    Releasing new version. (#755)

--- api-inference-community/setup.py
+++ api-inference-community/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="api_inference_community",
-    version="0.0.20",
+    version="0.0.21",
     description="A package with helper tools to build an API Inference docker app for Hugging Face API inference using huggingface_hub",
     url="http://github.com/huggingface/api-inference-community",
     author="Nicolas Patry",
@@ -15,11 +15,11 @@ setup(
     install_requires=list(line for line in open("requirements.txt", "r")),
     extras_require={
         "test": [
-            "httpx==0.18",
-            "Pillow==8.2",
-            "httpx==0.18",
-            "torch==1.9.0",
-            "pytest==6.2",
+            "httpx>=0.18",
+            "Pillow>=8.2",
+            "httpx>=0.18",
+            "torch>=1.9.0",
+            "pytest>=6.2",
         ]
     },
 )
