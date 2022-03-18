dc6ce038608c0c396fcf9bbad6c3e95ae1d71dd4
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2022-03-18 14:45:18 +0100

    New documentation

--- src/huggingface_hub/hub_mixin.py
+++ src/huggingface_hub/hub_mixin.py
@@ -23,7 +23,7 @@ class ModelHubMixin:
     """
     A Generic Base Model Hub Mixin. Define your own mixin for anything by inheriting from this class
     and overwriting _from_pretrained and _save_pretrained to define custom logic for saving/loading
-    your classes. See ``huggingface_hub.PyTorchModelHubMixin`` for an example.
+    your classes. See `huggingface_hub.PyTorchModelHubMixin` for an example.
     """
 
     def save_pretrained(
@@ -37,15 +37,13 @@ class ModelHubMixin:
         Saving weights in local directory.
 
         Parameters:
-            save_directory (:obj:`str`):
+            save_directory (`str`):
                 Specify directory in which you want to save weights.
-            config (:obj:`dict`, `optional`):
-                specify config (must be dict) incase you want to save it.
-            push_to_hub (:obj:`bool`, `optional`, defaults to :obj:`False`):
+            config (`dict`, *optional*):
+                specify config (must be dict) in case you want to save it.
+            push_to_hub (`bool`, *optional*, defaults to `False`):
                 Set it to `True` in case you want to push your weights to huggingface_hub
-            model_id (:obj:`str`, `optional`, defaults to :obj:`save_directory`):
-                Repo name in huggingface_hub. If not specified, repo name will be same as `save_directory`
-            kwargs (:obj:`Dict`, `optional`):
+            kwargs (`Dict`, *optional*):
                 kwargs will be passed to `push_to_hub`
         """
 
@@ -72,7 +70,7 @@ class ModelHubMixin:
     @classmethod
     def from_pretrained(
         cls,
-        pretrained_model_name_or_path: Optional[str],
+        pretrained_model_name_or_path: str,
         force_download: bool = False,
         resume_download: bool = False,
         proxies: Dict = None,
@@ -83,43 +81,45 @@ class ModelHubMixin:
     ):
         r"""
         Instantiate a pretrained pytorch model from a pre-trained model configuration from huggingface-hub.
-        The model is set in evaluation mode by default using ``model.eval()`` (Dropout modules are deactivated). To
-        train the model, you should first set it back in training mode with ``model.train()``.
+        The model is set in evaluation mode by default using `model.eval()` (Dropout modules are deactivated). To
+        train the model, you should first set it back in training mode with `model.train()`.
 
         Parameters:
-            pretrained_model_name_or_path (:obj:`str` or :obj:`os.PathLike`, `optional`):
+            pretrained_model_name_or_path (`str` or `os.PathLike`):
                 Can be either:
                     - A string, the `model id` of a pretrained model hosted inside a model repo on huggingface.co.
-                      Valid model ids can be located at the root-level, like ``bert-base-uncased``, or namespaced under
-                      a user or organization name, like ``dbmdz/bert-base-german-cased``.
-                    - You can add `revision` by appending `@` at the end of model_id simply like this: ``dbmdz/bert-base-german-cased@main``
+                      Valid model ids can be located at the root-level, like `bert-base-uncased`, or namespaced under
+                      a user or organization name, like `dbmdz/bert-base-german-cased`.
+                    - You can add `revision` by appending `@` at the end of model_id simply like this: `dbmdz/bert-base-german-cased@main`
                       Revision is the specific model version to use. It can be a branch name, a tag name, or a commit id,
-                      since we use a git-based system for storing models and other artifacts on huggingface.co, so ``revision`` can be any identifier allowed by git.
+                      since we use a git-based system for storing models and other artifacts on huggingface.co, so `revision` can be any identifier allowed by git.
                     - A path to a `directory` containing model weights saved using
-                      :func:`~transformers.PreTrainedModel.save_pretrained`, e.g., ``./my_model_directory/``.
-                    - :obj:`None` if you are both providing the configuration and state dictionary (resp. with keyword
-                      arguments ``config`` and ``state_dict``).
-            cache_dir (:obj:`Union[str, os.PathLike]`, `optional`):
+                      [`~transformers.PreTrainedModel.save_pretrained`], e.g., `./my_model_directory/`.
+                    - `None` if you are both providing the configuration and state dictionary (resp. with keyword
+                      arguments `config` and `state_dict`).
+            force_download (`bool`, *optional*, defaults to `False`):
+                Whether to force the (re-)download of the model weights and configuration files, overriding the
+                cached versions if they exist.
+            resume_download (`bool`, *optional*, defaults to `False`):
+                Whether to delete incompletely received files. Will attempt to resume the download if such a
+                file exists.
+            proxies (`Dict[str, str], *optional*):
+                A dictionary of proxy servers to use by protocol or endpoint, e.g., `{'http': 'foo.bar:3128',
+                'http://hostname': 'foo.bar:4012'}`. The proxies are used on each request.
+            use_auth_token (`str` or `bool`, *optional*):
+                The token to use as HTTP bearer authorization for remote files. If `True`, will use the token
+                generated when running `transformers-cli login` (stored in `~/.huggingface`).
+            cache_dir (`Union[str, os.PathLike]`, *optional*):
                 Path to a directory in which a downloaded pretrained model configuration should be cached if the
                 standard cache should not be used.
-            force_download (:obj:`bool`, `optional`, defaults to :obj:`False`):
-                Whether or not to force the (re-)download of the model weights and configuration files, overriding the
-                cached versions if they exist.
-            resume_download (:obj:`bool`, `optional`, defaults to :obj:`False`):
-                Whether or not to delete incompletely received files. Will attempt to resume the download if such a
-                file exists.
-            proxies (:obj:`Dict[str, str], `optional`):
-                A dictionary of proxy servers to use by protocol or endpoint, e.g., :obj:`{'http': 'foo.bar:3128',
-                'http://hostname': 'foo.bar:4012'}`. The proxies are used on each request.
-            local_files_only(:obj:`bool`, `optional`, defaults to :obj:`False`):
-                Whether or not to only look at local files (i.e., do not try to download the model).
-            use_auth_token (:obj:`str` or `bool`, `optional`):
-                The token to use as HTTP bearer authorization for remote files. If :obj:`True`, will use the token
-                generated when running :obj:`transformers-cli login` (stored in :obj:`~/.huggingface`).
-            model_kwargs (:obj:`Dict`, `optional`)::
+            local_files_only(`bool`, *optional*, defaults to `False`):
+                Whether to only look at local files (i.e., do not try to download the model).
+            model_kwargs (`Dict`, *optional*):
                 model_kwargs will be passed to the model during initialization
-        .. note::
-            Passing :obj:`use_auth_token=True` is required when you want to use a private model.
+
+        <Tip>
+        Passing `use_auth_token=True` is required when you want to use a private model.
+        </Tip>
         """
 
         model_id = pretrained_model_name_or_path
@@ -195,36 +195,36 @@ class ModelHubMixin:
     ) -> str:
         """
         Upload model checkpoint or tokenizer files to the 🤗 Model Hub while synchronizing a local clone of the repo in
-        :obj:`repo_path_or_name`.
+        `repo_path_or_name`.
 
         Parameters:
-            repo_path_or_name (:obj:`str`, `optional`):
+            repo_path_or_name (`str`, *optional*):
                 Can either be a repository name for your model or tokenizer in the Hub or a path to a local folder (in
                 which case the repository will have the name of that local folder). If not specified, will default to
-                the name given by :obj:`repo_url` and a local directory with that name will be created.
-            repo_url (:obj:`str`, `optional`):
+                the name given by `repo_url` and a local directory with that name will be created.
+            repo_url (`str`, *optional*):
                 Specify this in case you want to push to an existing repository in the hub. If unspecified, a new
-                repository will be created in your namespace (unless you specify an :obj:`organization`) with
-                :obj:`repo_name`.
-            commit_message (:obj:`str`, `optional`):
-                Message to commit while pushing. Will default to :obj:`"add config"`, :obj:`"add tokenizer"` or
-                :obj:`"add model"` depending on the type of the class.
-            organization (:obj:`str`, `optional`):
+                repository will be created in your namespace (unless you specify an `organization`) with
+                `repo_name`.
+            commit_message (`str`, *optional*):
+                Message to commit while pushing. Will default to `"add config"`, `"add tokenizer"` or
+                `"add model"` depending on the type of the class.
+            organization (`str`, *optional*):
                 Organization in which you want to push your model or tokenizer (you must be a member of this
                 organization).
-            private (:obj:`bool`, `optional`):
-                Whether or not the repository created should be private.
-            api_endpoint (:obj:`str`, `optional`):
+            private (`bool`, *optional*):
+                Whether the repository created should be private.
+            api_endpoint (`str`, *optional*):
                 The API endpoint to use when pushing the model to the hub.
-            use_auth_token (:obj:`bool` or :obj:`str`, `optional`):
-                The token to use as HTTP bearer authorization for remote files. If :obj:`True`, will use the token
-                generated when running :obj:`transformers-cli login` (stored in :obj:`~/.huggingface`). Will default to
-                :obj:`True` if :obj:`repo_url` is not specified.
-            git_user (``str``, `optional`):
-                will override the ``git config user.name`` for committing and pushing files to the hub.
-            git_email (``str``, `optional`):
-                will override the ``git config user.email`` for committing and pushing files to the hub.
-            config (:obj:`dict`, `optional`):
+            use_auth_token (`bool` or `str`, *optional*):
+                The token to use as HTTP bearer authorization for remote files. If `True`, will use the token
+                generated when running `transformers-cli login` (stored in `~/.huggingface`). Will default to
+                `True` if `repo_url` is not specified.
+            git_user (`str`, *optional*):
+                will override the `git config user.name` for committing and pushing files to the hub.
+            git_email (`str`, *optional*):
+                will override the `git config user.email` for committing and pushing files to the hub.
+            config (`dict`, *optional*):
                 Configuration object to be saved alongside the model weights.
 
 
