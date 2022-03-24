1d25b93f365b9226e64c561dba19765a7f59dbfd
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2022-03-24 16:03:29 +0100

    It was just another GitHub Actions issue

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -1370,6 +1370,7 @@ class HfApi:
         token: Optional[str] = None,
         organization: Optional[str] = None,
         repo_type: Optional[str] = None,
+        name: str = None,
     ) -> Dict[str, bool]:
         """Update the visibility setting of a repository.
 
