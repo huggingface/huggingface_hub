57e5a493712dd2369e6020e719fc3d7d275239d9
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2022-04-05 13:57:07 -0400

    Release guide

--- setup.py
+++ setup.py
@@ -1,4 +1,58 @@
 from setuptools import find_packages, setup
+"""
+Check list to release a new version.
+
+1. Checkout the main branch and pull the latest changes:
+   ```
+   git checkout main
+   git pull
+   ```
+
+2. Checkout a new branch with the version thar you'd like to release: v<VERSION>-release,
+   for example `v0.5.0-release` for a minor version and `v0.5.1-release` for a patch.
+
+3. Update the __version__ variable in the `src/huggingface_hub/__init__.py file to point 
+   to the version you're releasing:
+   ```
+   __version__ = "<VERSION>"
+   ```
+   
+4. Make sure that the conda build works correctly by building it locally:
+   ```
+   conda install -c defaults anaconda-client conda-build
+   HUB_VERSION=<VERSION> conda-build .github/conda
+   ```
+
+5. Make sure that the pip wheel works correctly by building it locally and installing it:
+   ```
+   pip install setuptools wheel
+   python setup.py sdist bdist_wheel
+   pip install dist/huggingface_hub-<VERSION>-py3-none-any.whl
+   ```
+   
+6. Push the branch:
+   ```
+   git push -u origin v<VERSION>-release
+   ```
+
+7. Verify that the docs have been built correctly. You can check that on the following link:
+   https://huggingface.co/docs/huggingface_hub/v<VERSION>
+   
+8. Checkout main once again to update the version in the __init__.py file:
+   ```
+   git checkout main
+   ```
+
+9. Update the version to contain the `.dev0` suffix:
+   ```
+   __version__ = "<VERSION+1>.dev0"  # For example, after releasing v0.5.0 or v0.5.1: "0.6.0.dev0".
+   ```
+   
+10. Push the changes!
+    ```
+    git push origin main
+    ```
+"""
 
 
 def get_version() -> str:
