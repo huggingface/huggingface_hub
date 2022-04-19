f22f41657eb407707a1a31b948b836bef0f3a0bb
leandro <leandro.vonwerra@spoud.io>
2022-04-19 11:20:04 +0200

    add tests

--- tests/test_repocard.py
+++ tests/test_repocard.py
@@ -13,14 +13,26 @@
 # limitations under the License.
 import os
 import shutil
+import time
 import unittest
 from pathlib import Path
 
+import pytest
+
+import yaml
 from huggingface_hub.constants import REPOCARD_NAME
-from huggingface_hub.repocard import metadata_eval_result, metadata_load, metadata_save
+from huggingface_hub.hf_api import HfApi
+from huggingface_hub.repocard import (
+    metadata_eval_result,
+    metadata_load,
+    metadata_save,
+    metadata_update,
+)
+from huggingface_hub.repository import Repository
 from huggingface_hub.utils import logging
 
-from .testing_utils import set_write_permission_and_retry
+from .testing_constants import ENDPOINT_STAGING, TOKEN, USER
+from .testing_utils import retry_endpoint, set_write_permission_and_retry
 
 
 ROUND_TRIP_MODELCARD_CASE = """
@@ -97,6 +109,8 @@ REPOCARD_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "fixtures/repocard"
 )
 
+REPO_NAME = "dummy-hf-hub-{}".format(int(time.time() * 10e3))
+
 
 class RepocardTest(unittest.TestCase):
     def setUp(self):
@@ -159,3 +173,142 @@ class RepocardTest(unittest.TestCase):
         metadata_save(filepath, data)
         content = filepath.read_text().splitlines()
         self.assertEqual(content, DUMMY_MODELCARD_EVAL_RESULT.splitlines())
+
+
+class RepocardUpdateTest(unittest.TestCase):
+    _api = HfApi(endpoint=ENDPOINT_STAGING)
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Share this valid token in all tests below.
+        """
+        cls._token = TOKEN
+        cls._api.set_access_token(TOKEN)
+
+    @retry_endpoint
+    def setUp(self) -> None:
+        if os.path.exists(REPO_NAME):
+            shutil.rmtree(REPO_NAME, onerror=set_write_permission_and_retry)
+        logger.info(f"Does {REPO_NAME} exist: {os.path.exists(REPO_NAME)}")
+        self.repo = Repository(
+            REPO_NAME,
+            clone_from=f"{USER}/{REPO_NAME}",
+            use_auth_token=self._token,
+            git_user="ci",
+            git_email="ci@dummy.com",
+        )
+
+        with self.repo.commit("Add README to main branch"):
+            with open("README.md", "w+") as f:
+                f.write(DUMMY_MODELCARD_EVAL_RESULT)
+
+        self.existing_metadata = yaml.safe_load(
+            DUMMY_MODELCARD_EVAL_RESULT.strip().strip("---")
+        )
+
+    def tearDown(self) -> None:
+        self._api.delete_repo(repo_id=f"{REPO_NAME}", token=self._token)  # {USER}/
+        shutil.rmtree(REPO_NAME)
+
+    def test_update_dataset_name(self):
+        new_datasets_data = {"datasets": "['test/test_dataset']"}
+        metadata_update(f"{USER}/{REPO_NAME}", new_datasets_data, token=self._token)
+
+        self.repo.git_pull()
+        updated_metadata = metadata_load(f"{REPO_NAME}/README.md")
+        expected_metadata = self.existing_metadata.copy()
+        expected_metadata.update(new_datasets_data)
+        self.assertDictEqual(updated_metadata, expected_metadata)
+
+    def test_update_existing_result_with_overwrite(self):
+        new_metadata = self.existing_metadata.copy()
+        new_metadata["model-index"][0]["results"][0]["metrics"][0][
+            "value"
+        ] = 0.2862102282047272
+        metadata_update(
+            f"{USER}/{REPO_NAME}", new_metadata, token=self._token, overwrite=True
+        )
+
+        self.repo.git_pull()
+        updated_metadata = metadata_load(f"{REPO_NAME}/README.md")
+        self.assertDictEqual(updated_metadata, new_metadata)
+
+    def test_update_existing_result_without_overwrite(self):
+        new_metadata = self.existing_metadata.copy()
+        new_metadata["model-index"][0]["results"][0]["metrics"][0][
+            "value"
+        ] = 0.2862102282047272
+
+        with pytest.raises(
+            ValueError,
+            match="You passed a new value for the existing metric 'Accuracy'. Set `overwrite=True` to overwrite existing metrics.",
+        ):
+            metadata_update(
+                f"{USER}/{REPO_NAME}", new_metadata, token=self._token, overwrite=False
+            )
+
+    def test_update_existing_field_without_overwrite(self):
+        new_datasets_data = {"datasets": "['test/test_dataset']"}
+        metadata_update(f"{USER}/{REPO_NAME}", new_datasets_data, token=self._token)
+
+        with pytest.raises(
+            ValueError,
+            match="You passed a new value for the existing meta data field 'datasets'. Set `overwrite=True` to overwrite existing metadata.",
+        ):
+            new_datasets_data = {"datasets": "['test/test_dataset_2']"}
+            metadata_update(
+                f"{USER}/{REPO_NAME}",
+                new_datasets_data,
+                token=self._token,
+                overwrite=False,
+            )
+
+    def test_update_new_result_existing_dataset(self):
+        new_result = metadata_eval_result(
+            model_pretty_name="RoBERTa fine-tuned on ReactionGIF",
+            task_pretty_name="Text Classification",
+            task_id="text-classification",
+            metrics_pretty_name="Recall",
+            metrics_id="recall",
+            metrics_value=0.7762102282047272,
+            dataset_pretty_name="ReactionGIF",
+            dataset_id="julien-c/reactiongif",
+        )
+
+        metadata_update(
+            f"{USER}/{REPO_NAME}", new_result, token=self._token, overwrite=False
+        )
+
+        expected_metadata = self.existing_metadata.copy()
+        expected_metadata["model-index"][0]["results"][0]["metrics"].append(
+            new_result["model-index"][0]["results"][0]["metrics"][0]
+        )
+
+        self.repo.git_pull()
+        updated_metadata = metadata_load(f"{REPO_NAME}/README.md")
+        self.assertDictEqual(updated_metadata, expected_metadata)
+
+    def test_update_new_result_new_dataset(self):
+        new_result = metadata_eval_result(
+            model_pretty_name="RoBERTa fine-tuned on ReactionGIF",
+            task_pretty_name="Text Classification",
+            task_id="text-classification",
+            metrics_pretty_name="Accuracy",
+            metrics_id="accuracy",
+            metrics_value=0.2662102282047272,
+            dataset_pretty_name="ReactionJPEG",
+            dataset_id="julien-c/reactionjpeg",
+        )
+
+        metadata_update(
+            f"{USER}/{REPO_NAME}", new_result, token=self._token, overwrite=False
+        )
+
+        expected_metadata = self.existing_metadata.copy()
+        expected_metadata["model-index"][0]["results"].append(
+            new_result["model-index"][0]["results"][0]
+        )
+        self.repo.git_pull()
+        updated_metadata = metadata_load(f"{REPO_NAME}/README.md")
+        self.assertDictEqual(updated_metadata, expected_metadata)
