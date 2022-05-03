b8faa73c9e4ba5c7a301d500dab3340662dfffe9
leandro <leandro.vonwerra@spoud.io>
2022-05-03 16:32:50 +0200

    don't allow empty commits if file hasn't  changed

--- src/huggingface_hub/repocard.py
+++ src/huggingface_hub/repocard.py
@@ -172,7 +172,7 @@ def metadata_update(
         path_in_repo="README.md",
         repo_id=repo_id,
         repo_type=repo_type,
-        identical_ok=True,
+        identical_ok=False,
         token=token,
     )
 
