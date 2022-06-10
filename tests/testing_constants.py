8eab5ca019ec7c5cfbb65e601cf6805f087e353e
SBrandeis <s.brandeis@protonmail.com>
2022-06-10 10:04:07 +0200

    🏗 Use `hub-ci` for tests
    cc @XCiD

--- tests/testing_constants.py
+++ tests/testing_constants.py
@@ -6,8 +6,8 @@ PASS = "__DUMMY_TRANSFORMERS_PASS__"
 TOKEN = "hf_94wBhPGp6KrrTH3KDchhKpRxZwd6dmHWLL"
 
 ENDPOINT_PRODUCTION = "https://huggingface.co"
-ENDPOINT_STAGING = "https://moon-staging.huggingface.co"
-ENDPOINT_STAGING_BASIC_AUTH = f"https://{USER}:{PASS}@moon-staging.huggingface.co"
+ENDPOINT_STAGING = "https://hub-ci.huggingface.co"
+ENDPOINT_STAGING_BASIC_AUTH = f"https://{USER}:{PASS}@hub-ci.huggingface.co"
 
 ENDPOINT_PRODUCTION_URL_SCHEME = (
     ENDPOINT_PRODUCTION + "/{repo_id}/resolve/{revision}/{filename}"
