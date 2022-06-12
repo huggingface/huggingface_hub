2b502a186d31cd8c7412ccf8ad1ebab05a6a55eb
Daniel van Strien <davanstrien@gmail.com>
2022-06-12 12:38:33 +0100

    use insert order for dict sorting

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -6,7 +6,7 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# §
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -227,7 +227,9 @@ class ModelInfo:
 
     def __repr__(self):
         s = f"{self.__class__.__name__}: {{ \n"
-        s += textwrap.indent(pprint.pformat(self.__dict__, width=70), "        ")
+        s += textwrap.indent(
+            pprint.pformat(self.__dict__, width=70, sort_dicts=False), "        "
+        )
         s += "\n\t}"
         return s
 
