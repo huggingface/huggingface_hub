d0a5ed9aef183d0c3dca79b68b149a3f37f6aceb
SBrandeis <s.brandeis@protonmail.com>
2022-06-17 18:52:43 +0200

    🔥 Remove FileSlice once again

--- src/huggingface_hub/commands/lfs.py
+++ src/huggingface_hub/commands/lfs.py
@@ -120,50 +120,6 @@ def read_msg() -> Optional[Dict]:
     return msg
 
 
-class FileSlice(AbstractContextManager):
-    """
-    DEPRECATED - will be removed in 0.11.0
-    Use `huggingface_hub.lfs.SliceFileObj` instead
-
-    File-like object that only reads a slice of a file
-    Inspired by stackoverflow.com/a/29838711/593036
-    """
-
-    def __init__(self, filepath: str, seek_from: int, read_limit: int):
-        warnings.warn(
-            "FileSlice is deprecated and will be removed in 0.11.0. Use"
-            " huggingface_hub.lfs.SliceFileObj instead.",
-            FutureWarning,
-        )
-        self.filepath = filepath
-        self.seek_from = seek_from
-        self.read_limit = read_limit
-        self.n_seen = 0
-
-    def __enter__(self):
-        self.f = open(self.filepath, "rb")
-        self.f.seek(self.seek_from)
-        return self
-
-    def __len__(self):
-        total_length = os.fstat(self.f.fileno()).st_size
-        return min(self.read_limit, total_length - self.seek_from)
-
-    def read(self, n=-1):
-        if self.n_seen >= self.read_limit:
-            return b""
-        remaining_amount = self.read_limit - self.n_seen
-        data = self.f.read(remaining_amount if n < 0 else min(n, remaining_amount))
-        self.n_seen += len(data)
-        return data
-
-    def __iter__(self):
-        yield self.read(n=4 * 1024 * 1024)
-
-    def __exit__(self, *args):
-        self.f.close()
-
-
 class LfsUploadCommand:
     def __init__(self, args):
         self.args = args
