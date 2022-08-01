d11e62673c943bb1d76df67bca63520cce8f9362
merveenoyan <merveenoyan@gmail.com>
2022-08-01 16:35:16 +0200

    Revert "modified test to skip repo creation during setup"
    This reverts commit da18a5a031acac9a9b80ef15664e05767d655cea.

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -81,19 +81,16 @@ class RepositoryTest(RepositoryCommonTest):
         logger.info(
             f"Does {WORKING_REPO_DIR} exist: {os.path.exists(WORKING_REPO_DIR)}"
         )
-
         self.REPO_NAME = repo_name()
-        # skip creation of repository for clone from test
-        if self.shortDescription() != "SKIP CREATION":
-            self._repo_url = self._api.create_repo(
-                repo_id=self.REPO_NAME, token=self._token
-            )
-            self._api.upload_file(
-                path_or_fileobj=BytesIO(b"some initial binary data: \x00\x01"),
-                path_in_repo="random_file.txt",
-                repo_id=f"{USER}/{self.REPO_NAME}",
-                token=self._token,
-            )
+        self._repo_url = self._api.create_repo(
+            repo_id=self.REPO_NAME, token=self._token
+        )
+        self._api.upload_file(
+            path_or_fileobj=BytesIO(b"some initial binary data: \x00\x01"),
+            path_in_repo="random_file.txt",
+            repo_id=f"{USER}/{self.REPO_NAME}",
+            token=self._token,
+        )
 
     def tearDown(self):
         try:
@@ -152,7 +149,6 @@ class RepositoryTest(RepositoryCommonTest):
                 WORKING_REPO_DIR,
                 clone_from=f"{USER}/{uuid.uuid4()}",
                 use_auth_token=self._token,
-                repo_type="dataset",
             )
 
     def test_clone_from_model(self):
