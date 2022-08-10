b0dd0134f5a5e861dad3b4d1d2de13d05087f6bb
nateraw <nxr9266@g.rit.edu>
2022-08-10 18:31:45 -0400

    :memo: fix doctests even though we aren't running them

--- src/huggingface_hub/repocard_data.py
+++ src/huggingface_hub/repocard_data.py
@@ -385,7 +385,7 @@ def eval_results_to_model_index(model_name: str, eval_results: List[EvalResult])
         ...     )
         ... ]
         >>> eval_results_to_model_index("my-cool-model", eval_results)
-        [{'name': 'my-cool-model', 'results': [{'task': {'type': 'image-classification'}, 'dataset': {'type': 'beans', 'name': 'Beans'}, 'metrics': [{'type': 'accuracy', 'value': 0.9}]}]}]
+        [{'name': 'my-cool-model', 'results': [{'task': {'type': 'image-classification'}, 'dataset': {'name': 'Beans', 'type': 'beans'}, 'metrics': [{'type': 'accuracy', 'value': 0.9}]}]}]
 
     """
 
