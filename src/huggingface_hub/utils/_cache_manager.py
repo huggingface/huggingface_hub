4dc4ece27b098385a11f8ec11a21e64e986a8ad0
Lucain Pouget <lucainp@gmail.com>
2022-09-20 17:01:34 +0200

    support non-symlink in scan cache

--- src/huggingface_hub/utils/_cache_manager.py
+++ src/huggingface_hub/utils/_cache_manager.py
@@ -679,20 +679,10 @@ def _scan_cached_repo(repo_path: Path) -> CachedRepoInfo:
             if file_path.is_dir():
                 continue
 
-            if not file_path.is_symlink():
-                raise CorruptedCacheException(
-                    f"Revision folder corrupted. Found a non-symlink file: {file_path}"
-                )
-
             blob_path = Path(file_path).resolve()
             if not blob_path.exists():
                 raise CorruptedCacheException(
                     f"Blob missing (broken symlink): {blob_path}"
-                )
-
-            if blobs_path not in blob_path.parents:
-                raise CorruptedCacheException(
-                    f"Blob symlink points outside of blob directory: {blob_path}"
                 )
 
             if blob_path not in blob_stats:
