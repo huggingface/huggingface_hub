8f5ec279a42e703246e7f76e9e642925e9d87cf4
Lucain Pouget <lucainp@gmail.com>
2022-09-29 12:21:36 +0200

    another create_repo

--- tests/test_snapshot_download.py
+++ tests/test_snapshot_download.py
@@ -35,11 +35,11 @@ class SnapshotDownloadTests(unittest.TestCase):
         cls._api.set_access_token(TOKEN)
 
     @retry_endpoint
-    @expect_deprecation("clone_from")
     def setUp(self) -> None:
         if os.path.exists(REPO_NAME):
             shutil.rmtree(REPO_NAME, onerror=set_write_permission_and_retry)
         logger.info(f"Does {REPO_NAME} exist: {os.path.exists(REPO_NAME)}")
+        self._api.create_repo(f"{USER}/{REPO_NAME}", token=self._token)
         repo = Repository(
             REPO_NAME,
             clone_from=f"{USER}/{REPO_NAME}",
