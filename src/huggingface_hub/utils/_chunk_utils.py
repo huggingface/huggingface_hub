76a73bb4a1c9012d565629f3887446bcf036b802
Lucain Pouget <lucainp@gmail.com>
2022-10-18 14:28:08 +0200

    refacto chunk iterator

--- src/huggingface_hub/utils/_chunk_utils.py
+++ src/huggingface_hub/utils/_chunk_utils.py
@@ -1,12 +1,30 @@
-"""Miscellaneous / general utilities"""
-from typing import Any, Iterable, Iterator, List
+# coding=utf-8
+# Copyright 2022-present, the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Contains a utility to iterate by chunks over an iterator."""
+import itertools
+from typing import Iterable, TypeVar
 
 
-def chunk_iterable(
-    iterable: Iterable[Any],
-    chunk_size: int,
-) -> Iterator[List[Any]]:
+T = TypeVar("T")
+
+
+def chunk_iterable(iterable: Iterable[T], chunk_size: int) -> Iterable[Iterable[T]]:
     """Iterates over an iterator chunk by chunk.
+
+    Taken from https://stackoverflow.com/a/8998040.
+    See also https://github.com/huggingface/huggingface_hub/pull/920#discussion_r938793088.
 
     Args:
         iterable (`Iterable`):
@@ -34,14 +52,13 @@ def chunk_iterable(
         The last chunk can be smaller than `chunk_size`.
     </Tip>
     """
+    if not isinstance(chunk_size, int) or chunk_size <= 0:
+        raise ValueError("`chunk_size` must be a strictly positive integer (>0).")
 
-    def _chunk_iter(itr: Iterable[Any]) -> Iterator[List[Any]]:
-        while True:
-            chunk = [x for _, x in zip(range(chunk_size), itr)]
-            if not chunk:
-                break
-            yield chunk
-
-    if not chunk_size > 0:
-        raise ValueError("chunk_size must be a strictly positive (>0) integer")
-    return _chunk_iter(iter(iterable))
+    iterator = iter(iterable)
+    while True:
+        try:
+            next_item = next(iterator)
+        except StopIteration:
+            return
+        yield itertools.chain((next_item,), itertools.islice(iterator, chunk_size - 1))
--- tests/test_utils_chunks.py
+++ tests/test_utils_chunks.py
@@ -8,19 +8,21 @@ class TestUtilsCommon(unittest.TestCase):
         # Can iterable over any iterable (iterator, list, tuple,...)
         for iterable in (range(12), list(range(12)), tuple(range(12))):
             # 12 is a multiple of 4 -> last chunk is not truncated
-            self.assertListEqual(
-                list(chunk_iterable(iterable, chunk_size=4)),
+            for chunk, expected_chunk in zip(
+                chunk_iterable(iterable, chunk_size=4),
                 [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]],
-            )
+            ):
+                self.assertListEqual(list(chunk), expected_chunk)
 
     def test_chunk_iterable_last_chunk_truncated(self):
         # Can iterable over any iterable (iterator, list, tuple,...)
         for iterable in (range(12), list(range(12)), tuple(range(12))):
             # 12 is NOT a multiple of 5 -> last chunk is truncated
-            self.assertListEqual(
-                list(chunk_iterable(iterable, chunk_size=5)),
+            for chunk, expected_chunk in zip(
+                chunk_iterable(iterable, chunk_size=5),
                 [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11]],
-            )
+            ):
+                self.assertListEqual(list(chunk), expected_chunk)
 
     def test_chunk_iterable_validation(self):
         with self.assertRaises(ValueError):
