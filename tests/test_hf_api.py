ccad4cdd12a6c2df9234a7fd604c2c62414ba8a3
Lucain <lucainp@gmail.com>
2022-10-26 17:54:54 +0200

    Fix deprecation warnings for assertEquals in tests (#1135)

--- tests/test_hf_api.py
+++ tests/test_hf_api.py
@@ -2070,4 +2070,4 @@ class HfApiTokenAttributeTest(unittest.TestCase):
     def _assert_token_is(
         self, mock_build_hf_headers: Mock, expected_value: str
     ) -> None:
-        self.assertEquals(mock_build_hf_headers.call_args[1]["token"], expected_value)
+        self.assertEqual(mock_build_hf_headers.call_args[1]["token"], expected_value)
