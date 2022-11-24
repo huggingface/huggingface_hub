12aa4bac343ebf2f55e0f83cc66e8fd685a29cd3
Lucain Pouget <lucainp@gmail.com>
2022-11-24 15:51:06 +0100

    ADapt timm tests

--- contrib/timm/test_timm.py
+++ contrib/timm/test_timm.py
@@ -1,11 +1,19 @@
 import timm
 
+from ..utils import production_endpoint
 
-def test_push_to_hub(repo_name: str, cleanup_repo: None) -> None:
-    # Build a model 🔧
-    model = timm.create_model("resnet18", pretrained=True, num_classes=4)
 
-    # Push it to the 🤗 hub
-    timm.models.hub.push_to_hf_hub(
-        model, repo_name, model_config=dict(labels=["a", "b", "c", "d"])
-    )
+MODEL_ID = "nateraw/timm-resnet50-beans"
+
+
+def test_load_and_push_to_hub(repo_name: str, cleanup_repo: None) -> None:
+    # Test load only config
+    with production_endpoint():
+        _ = timm.models.hub.load_model_config_from_hf(MODEL_ID)
+
+    # Load entire model from Hub
+    with production_endpoint():
+        model = timm.create_model("hf_hub:" + MODEL_ID, pretrained=True)
+
+    # Push model to Hub
+    timm.models.hub.push_to_hf_hub(model, repo_name)
