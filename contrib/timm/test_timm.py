173aff85bd26254a9a2d0d2cbc14f0d7c1f355f0
Lucain Pouget <lucainp@gmail.com>
2022-11-25 13:19:27 +0100

    Include feedback form osanseviero

--- contrib/timm/test_timm.py
+++ contrib/timm/test_timm.py
@@ -6,14 +6,15 @@ from ..utils import production_endpoint
 MODEL_ID = "nateraw/timm-resnet50-beans"
 
 
-def test_load_and_push_to_hub(repo_name: str, cleanup_repo: None) -> None:
+@production_endpoint()
+def test_load_from_hub() -> None:
     # Test load only config
-    with production_endpoint():
-        _ = timm.models.hub.load_model_config_from_hf(MODEL_ID)
+    _ = timm.models.hub.load_model_config_from_hf(MODEL_ID)
 
     # Load entire model from Hub
-    with production_endpoint():
-        model = timm.create_model("hf_hub:" + MODEL_ID, pretrained=True)
+    _ = timm.create_model("hf_hub:" + MODEL_ID, pretrained=True)
 
-    # Push model to Hub
+
+def test_push_to_hub(repo_name: str, cleanup_repo: None) -> None:
+    model = timm.create_model("resnet18")
     timm.models.hub.push_to_hf_hub(model, repo_name)
