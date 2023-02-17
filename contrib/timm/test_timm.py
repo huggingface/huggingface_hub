cf0ab59c6aca7c56a6be2d8c07f8ba47c4d84fe9
Lucain <lucainp@gmail.com>
2023-02-17 10:43:38 +0100

    Fix contrib CI for timm (#1346)
    * Fix timm CI
    
    * update gh ci for contrib

--- contrib/timm/test_timm.py
+++ contrib/timm/test_timm.py
@@ -3,18 +3,18 @@ import timm
 from ..utils import production_endpoint
 
 
-MODEL_ID = "nateraw/timm-resnet50-beans"
+MODEL_ID = "timm/mobilenetv3_large_100.ra_in1k"
 
 
 @production_endpoint()
 def test_load_from_hub() -> None:
     # Test load only config
-    _ = timm.models.hub.load_model_config_from_hf(MODEL_ID)
+    _ = timm.models.load_model_config_from_hf(MODEL_ID)
 
     # Load entire model from Hub
     _ = timm.create_model("hf_hub:" + MODEL_ID, pretrained=True)
 
 
 def test_push_to_hub(repo_name: str, cleanup_repo: None) -> None:
-    model = timm.create_model("resnet18")
-    timm.models.hub.push_to_hf_hub(model, repo_name)
+    model = timm.create_model("mobilenetv3_rw")
+    timm.models.push_to_hf_hub(model, repo_name)
