d6f5f58ba998c318b1c391ad1bd699a5913bf43e
Luc Georges <luc@huggingface.co>
2023-03-16 21:43:51 +0100

    feat: update `hf_transfer.upload` function args

--- src/huggingface_hub/_commit_api.py
+++ src/huggingface_hub/_commit_api.py
@@ -395,7 +395,7 @@ def _upload_lfs_object(operation: CommitOperationAdd, lfs_batch_action: dict, to
             from hf_transfer import upload
 
             logger.debug(f"Uploading {operation.path_in_repo} as LFS file...")
-            upload(operation.path_or_fileobj, upload_action, verify_action, upload_info, token, max_files=128, parallel_failures=127, max_retries=5)
+            upload(operation.path_or_fileobj, upload_action, verify_action, upload_info.sha256, upload_info.size, token, max_files=128, parallel_failures=127, max_retries=5)
             logger.debug(f"{operation.path_in_repo}: Upload successful")
             return
         except ImportError:
