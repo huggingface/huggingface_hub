c7777f726b17f1648e33ec4fccb47d7bf57f9edb
Lucain Pouget <lucainp@gmail.com>
2023-05-24 17:09:04 +0200

    Push ot hub on context manager closing

--- src/huggingface_hub/_tensorboard_logger.py
+++ src/huggingface_hub/_tensorboard_logger.py
@@ -15,3 +15,9 @@ class HFTensorBoardLogger(HFLoggerMixin, SummaryWriter):
     @property
     def log_dir(self) -> str:
         return self.logdir
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Push to hub in a non-blocking way when exiting the logger's context manager."""
+        super().__exit__(exc_type, exc_val, exc_tb)
+        future = self.push_to_hub(commit_message="Closing Tensorboard logger.")
+        future.result()
