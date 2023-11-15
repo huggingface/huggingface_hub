f12e9c3d88138b5fde489f55b29eb737c5f8373f
Lucain <lucainp@gmail.com>
2023-11-15 09:57:34 +0100

    Update src/huggingface_hub/hf_api.py
    Co-authored-by: Steven Liu <59462357+stevhliu@users.noreply.github.com>

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -382,7 +382,7 @@ class RepoSibling:
 
     All attributes of this class are optional except `rfilename`. This is because only the file names are returned when
     listing repositories on the Hub (with [`list_models`], [`list_datasets`] or [`list_spaces`]). If you need more
-    information like file size, blob id or lfs details, you must request them specifically for one repo at a time
+    information like file size, blob id or lfs details, you must request them specifically from one repo at a time
     (using [`model_info`], [`dataset_info`] or [`space_info`]) as it adds more constraints on the backend server to
     retrieve these.
 
