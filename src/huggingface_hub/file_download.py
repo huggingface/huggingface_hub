7ded427715b4333fe5885976c4ee355af1583d5f
Scruel Tao <me@scruel.com>
2024-01-16 23:55:01 +0800

    fix: actual error won't be raised while `force_download=True`

--- src/huggingface_hub/file_download.py
+++ src/huggingface_hub/file_download.py
@@ -1338,6 +1338,10 @@ def hf_hub_download(
             head_call_error = error
             pass
 
+    # Repo not found or gated => let's raise the actual error
+    raise_actual_error = (isinstance(head_call_error, RepositoryNotFoundError) 
+                          or isinstance(head_call_error, GatedRepoError))
+
     # etag can be None for several reasons:
     # 1. we passed local_files_only.
     # 2. we don't have a connection
@@ -1351,6 +1355,8 @@ def hf_hub_download(
     if etag is None:
         # In those cases, we cannot force download.
         if force_download:
+            if raise_actual_error:
+                raise head_call_error
             raise ValueError(
                 "We have no connection or you passed local_files_only, so force_download is not an accepted option."
             )
@@ -1384,8 +1390,7 @@ def hf_hub_download(
                 "Cannot find the requested files in the disk cache and outgoing traffic has been disabled. To enable"
                 " hf.co look-ups and downloads online, set 'local_files_only' to False."
             )
-        elif isinstance(head_call_error, RepositoryNotFoundError) or isinstance(head_call_error, GatedRepoError):
-            # Repo not found => let's raise the actual error
+        elif raise_actual_error:
             raise head_call_error
         else:
             # Otherwise: most likely a connection issue or Hub downtime => let's warn the user
