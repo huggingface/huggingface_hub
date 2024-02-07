616a21985d76056eb2990d2d3dcb7ae50262c875
ahmdayyy <ahmednizamani36.com>
2024-02-07 20:17:31 +0100

    added audio_to_audio in inference client

--- src/huggingface_hub/inference/_client.py
+++ src/huggingface_hub/inference/_client.py
@@ -33,6 +33,7 @@
 # - Images are parsed as PIL.Image for easier manipulation.
 # - Provides a "recommended model" for each task => suboptimal but user-wise quicker to get a first script running.
 # - Only the main parameters are publicly exposed. Power users can always read the docs for more options.
+import base64
 import logging
 import time
 import warnings
@@ -1988,3 +1989,50 @@ class InferenceClient:
             compute_type=response_data["compute_type"],
             framework=response_data["framework"],
         )
+
+    def audio_to_audio(
+        self,
+        audio: ContentT,
+        *,
+        model: Optional[str] = None,
+    ) -> List[bytes]:
+        """
+        Performs multiple tasks related to audio-to-audio depending on the model (eg: speech enhancement, source separation).
+
+        Args:
+            audio (Union[str, Path, bytes, BinaryIO]):
+                The audio content for the model. It can be raw audio bytes, a local audio file, or a URL pointing to an
+                audio file.
+            model (`str`, *optional*):
+                The model can be any model which takes an audio file and returns another audio file. Can be a model ID hosted on the Hugging Face Hub
+                or a URL to a deployed Inference Endpoint. If not provided, the default recommended model for
+                audio classification will be used.
+
+        Returns:
+            `List[bytes]`: A list of dictionary containing 'label', 'content-type' and audio in 'blob'.
+
+        Raises:
+            `InferenceTimeoutError`:
+                If the model is unavailable or the request times out.
+            `HTTPError`:
+                If the request fails with an HTTP error status code other than HTTP 503.
+
+        Example:
+        ```py
+        >>> from huggingface_hub import InferenceClient
+        >>> client = InferenceClient()
+        >>> audio_output = client.audio_to_audio("audio.flac")
+        >>> for i, item in enumerate(output):
+        >>>     with open(f"output_{i}.flac", "wb") as f:
+                    f.write(item["blob"])
+        ```
+        """
+        response = self.post(data=audio, model=model, task="audio-to-audio")
+        response = _bytes_to_list(response)
+        audio_output = []
+        for item in response:
+            audio_output.append({'label':item['label'],'content-type':item['content-type'],'blob':base64.b64decode(item['blob'])})
+        return audio_output
+
+
+
