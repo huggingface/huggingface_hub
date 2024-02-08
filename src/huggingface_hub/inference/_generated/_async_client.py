95c6d7fba5d7175bd2435d3ba2ecc7a9afea9c16
ahmdayyy <ahmednizamani36.com>
2024-02-08 18:24:24 +0100

    updated audio_to_audio by adding typed dict

--- src/huggingface_hub/inference/_generated/_async_client.py
+++ src/huggingface_hub/inference/_generated/_async_client.py
@@ -64,6 +64,7 @@ from huggingface_hub.inference._text_generation import (
     raise_text_generation_error,
 )
 from huggingface_hub.inference._types import (
+    AudioToAudioOutput,
     ClassificationOutput,
     ConversationalOutput,
     FillMaskOutput,
@@ -295,6 +296,50 @@ class AsyncInferenceClient:
         """
         response = await self.post(data=audio, model=model, task="audio-classification")
         return _bytes_to_list(response)
+
+    async def audio_to_audio(
+        self,
+        audio: ContentT,
+        *,
+        model: Optional[str] = None,
+    ) -> List[AudioToAudioOutput]:
+        """
+        Performs multiple tasks related to audio-to-audio depending on the model (eg: speech enhancement, source separation).
+
+        Args:
+            audio (Union[str, Path, bytes, BinaryIO]):
+                The audio content for the model. It can be raw audio bytes, a local audio file, or a URL pointing to an
+                audio file.
+            model (`str`, *optional*):
+                The model can be any model which takes an audio file and returns another audio file. Can be a model ID hosted on the Hugging Face Hub
+                or a URL to a deployed Inference Endpoint. If not provided, the default recommended model for
+                audio_to_audio will be used.
+
+        Returns:
+            `List[Dict]`: A list of dictionary where each index contains audios label, content-type, and audio content in blob.
+
+        Raises:
+            `InferenceTimeoutError`:
+                If the model is unavailable or the request times out.
+            `aiohttp.ClientResponseError`:
+                If the request fails with an HTTP error status code other than HTTP 503.
+
+        Example:
+        ```py
+        # Must be run in an async context
+        >>> from huggingface_hub import AsyncInferenceClient
+        >>> client = AsyncInferenceClient()
+        >>> audio_output = await client.audio_to_audio("audio.flac")
+        >>> async for i, item in enumerate(output):
+        >>>     with open(f"output_{i}.flac", "wb") as f:
+                    f.write(item["blob"])
+        ```
+        """
+        response = await self.post(data=audio, model=model, task="audio-to-audio")
+        audio_output = _bytes_to_list(response)
+        for item in audio_output:
+            item["blob"] = base64.b64decode(item["blob"])
+        return audio_output
 
     async def automatic_speech_recognition(
         self,
@@ -2021,50 +2066,3 @@ class AsyncInferenceClient:
             compute_type=response_data["compute_type"],
             framework=response_data["framework"],
         )
-
-    async def audio_to_audio(
-        self,
-        audio: ContentT,
-        *,
-        model: Optional[str] = None,
-    ) -> List[bytes]:
-        """
-        Performs multiple tasks related to audio-to-audio depending on the model (eg: speech enhancement, source separation).
-
-        Args:
-            audio (Union[str, Path, bytes, BinaryIO]):
-                The audio content for the model. It can be raw audio bytes, a local audio file, or a URL pointing to an
-                audio file.
-            model (`str`, *optional*):
-                The model can be any model which takes an audio file and returns another audio file. Can be a model ID hosted on the Hugging Face Hub
-                or a URL to a deployed Inference Endpoint. If not provided, the default recommended model for
-                audio classification will be used.
-
-        Returns:
-            `List[bytes]`: A list of dictionary containing 'label', 'content-type' and audio in 'blob'.
-
-        Raises:
-            `InferenceTimeoutError`:
-                If the model is unavailable or the request times out.
-            `aiohttp.ClientResponseError`:
-                If the request fails with an HTTP error status code other than HTTP 503.
-
-        Example:
-        ```py
-        # Must be run in an async context
-        >>> from huggingface_hub import AsyncInferenceClient
-        >>> client = AsyncInferenceClient()
-        >>> audio_output = await client.audio_to_audio("audio.flac")
-        >>> async for i, item in enumerate(output):
-        >>>     with open(f"output_{i}.flac", "wb") as f:
-                    f.write(item["blob"])
-        ```
-        """
-        response = await self.post(data=audio, model=model, task="audio-to-audio")
-        response = _bytes_to_list(response)
-        audio_output = []
-        for item in response:
-            audio_output.append(
-                {"label": item["label"], "content-type": item["content-type"], "blob": base64.b64decode(item["blob"])}
-            )
-        return audio_output
