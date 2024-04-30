03546dda4d96b8a045a6ab0b255f857c7c1692af
Lucain Pouget <lucainp@gmail.com>
2024-04-30 17:15:36 +0200

    better

--- src/huggingface_hub/inference/_client.py
+++ src/huggingface_hub/inference/_client.py
@@ -1790,8 +1790,8 @@ class InferenceClient:
 
         <Tip>
 
-        If you have the model to generate a response based on chat messages, you should use the [`InferenceClient.chat_completion`] method.
-        This method accepts a list of messages instead of a single text prompt and handles the chat templating for you.
+        If you want to generate a response from chat messages, you should use the [`InferenceClient.chat_completion`] method.
+        It accepts a list of messages instead of a single text prompt and handles the chat templating for you.
 
         </Tip>
 
