23f3aaeeda9dc17886e70eb96456fc6a5cf5af93
gorold <woogerald@yahoo.com.sg>
2024-05-29 12:23:34 +0800

    update HubMixinTest with union and optional custom type

--- tests/test_hub_mixin.py
+++ tests/test_hub_mixin.py
@@ -136,11 +136,22 @@ class DummyModelWithCustomTypes(
     },
 ):
     def __init__(
-        self, foo: int, bar: str, custom: CustomType, custom_default: CustomType = CustomType("default"), **kwargs
+        self,
+        foo: int,
+        bar: str,
+        baz: Union[int, str],
+        custom: CustomType,
+        optional_custom_1: Optional[CustomType],
+        optional_custom_2: Optional[CustomType],
+        custom_default: CustomType = CustomType("default"),
+        **kwargs,
     ):
         self.foo = foo
         self.bar = bar
+        self.baz = baz
         self.custom = custom
+        self.optional_custom_1 = optional_custom_1
+        self.optional_custom_2 = optional_custom_2
         self.custom_default = custom_default
 
     @classmethod
@@ -398,19 +409,32 @@ class HubMixinTest(unittest.TestCase):
         assert not hasattr(model, "other")
 
     def test_from_cls_with_custom_type(self):
-        model = DummyModelWithCustomTypes(1, bar="bar", custom=CustomType("custom"))
+        model = DummyModelWithCustomTypes(
+            1,
+            bar="bar",
+            baz=1.0,
+            custom=CustomType("custom"),
+            optional_custom_1=CustomType("optional"),
+            optional_custom_2=None,
+        )
         model.save_pretrained(self.cache_dir)
 
         config = json.loads((self.cache_dir / "config.json").read_text())
         assert config == {
             "foo": 1,
             "bar": "bar",
+            "baz": 1.0,
             "custom": {"value": "custom"},
+            "optional_custom_1": {"value": "optional"},
+            "optional_custom_2": None,
             "custom_default": {"value": "default"},
         }
 
         model_reloaded = DummyModelWithCustomTypes.from_pretrained(self.cache_dir)
         assert model_reloaded.foo == 1
         assert model_reloaded.bar == "bar"
+        assert model_reloaded.baz == 1.0
         assert model_reloaded.custom.value == "custom"
+        assert model_reloaded.optional_custom_1 is not None and model_reloaded.optional_custom_1.value == "optional"
+        assert model_reloaded.optional_custom_2 is None
         assert model_reloaded.custom_default.value == "default"
