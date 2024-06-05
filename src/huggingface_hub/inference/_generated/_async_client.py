77d5263559e350620a9b24e9ae3022a499b88407
Lucain Pouget <lucainp@gmail.com>
2024-06-05 12:08:07 +0200

    Fix  always None in text_generation output

--- src/huggingface_hub/inference/_generated/_async_client.py
+++ src/huggingface_hub/inference/_generated/_async_client.py
@@ -2026,6 +2026,7 @@ class AsyncInferenceClient:
         parameters = {
             "best_of": best_of,
             "decoder_input_details": decoder_input_details,
+            "details": details,
             "do_sample": do_sample,
             "frequency_penalty": frequency_penalty,
             "grammar": grammar,
