af08444f040d1d7b7bfaba3fd89647085219b3ae
gorold <woogerald@yahoo.com.sg>
2024-06-11 15:02:07 +0800

    Only branch to pipe tests when version >= python3.10

--- tests/test_utils_typing.py
+++ tests/test_utils_typing.py
@@ -67,29 +67,11 @@ def test_is_jsonable_failure(data):
         (int, False),
         (None, False),
         (Union[int, float, None], False),
-        (Union[Union[int | float], None], False),
-        (Optional[int | float], False),
+        (Union[Union[int, float], None], False),
+        (Optional[Union[int, float]], False),
     ],
 )
 def test_is_simple_optional_type(type_: Type, is_optional: bool):
-    assert is_simple_optional_type(type_) is is_optional
-
-
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires python3.10 or higher")
-@pytest.mark.parametrize(
-    "type_, is_optional",
-    [
-        (int | None, True),
-        (None | int, True),
-        (CustomType | None, True),
-        (None | CustomType, True),
-        (int | float, False),
-        (int | float | None, False),
-        ((int | float) | None, False),
-        (Union[int, float] | None, False),
-    ],
-)
-def test_is_simple_optional_type_pipe(type_: Type, is_optional: bool):
     assert is_simple_optional_type(type_) is is_optional
 
 
@@ -114,15 +96,32 @@ def test_unwrap_simple_optional_type_fail(non_optional_type: Type):
         unwrap_simple_optional_type(non_optional_type)
 
 
-@pytest.mark.skipif(sys.version_info < (3, 10), reason="requires python3.10 or higher")
-@pytest.mark.parametrize(
-    "optional_type, inner_type",
-    [
-        (None | int, int),
-        (int | None, int),
-        (None | CustomType, CustomType),
-        (CustomType | None, CustomType),
-    ],
-)
-def test_unwrap_simple_optional_type_pipe(optional_type: Type, inner_type: Type):
-    assert unwrap_simple_optional_type(optional_type) is inner_type
+if sys.version_info >= (3, 10):
+    # Tests which require python3.10 or higher.
+    @pytest.mark.parametrize(
+        "type_, is_optional",
+        [
+            (int | None, True),
+            (None | int, True),
+            (CustomType | None, True),
+            (None | CustomType, True),
+            (int | float, False),
+            (int | float | None, False),
+            ((int | float) | None, False),
+            (Union[int, float] | None, False),
+        ],
+    )
+    def test_is_simple_optional_type_pipe(type_: Type, is_optional: bool):
+        assert is_simple_optional_type(type_) is is_optional
+
+    @pytest.mark.parametrize(
+        "optional_type, inner_type",
+        [
+            (None | int, int),
+            (int | None, int),
+            (None | CustomType, CustomType),
+            (CustomType | None, CustomType),
+        ],
+    )
+    def test_unwrap_simple_optional_type_pipe(optional_type: Type, inner_type: Type):
+        assert unwrap_simple_optional_type(optional_type) is inner_type
