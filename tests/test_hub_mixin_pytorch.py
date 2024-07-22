cd6d5f736a87c022dc3cdce1454a4781d2aede9c
Lucain Pouget <lucainp@gmail.com>
2024-07-22 09:06:17 +0200

    fix failed test

--- tests/test_hub_mixin_pytorch.py
+++ tests/test_hub_mixin_pytorch.py
@@ -307,7 +307,7 @@ class PytorchHubMixinTest(unittest.TestCase):
         DummyModel().push_to_hub(repo_id=repo_id, token=TOKEN, config=CONFIG)
 
         # Test model id exists
-        assert self._api.model_info(repo_id) == repo_id
+        assert self._api.model_info(repo_id).id == repo_id
 
         # Test config has been pushed to hub
         tmp_config_path = hf_hub_download(
