366576f97b98fbadab95f53c4732c8ccf22ed12d
Lucain Pouget <lucainp@gmail.com>
2024-08-19 15:06:07 +0200

    quality

--- tests/test_inference_async_client.py
+++ tests/test_inference_async_client.py
@@ -218,7 +218,7 @@ async def test_async_chat_completion_with_stream() -> None:
         generated_text += item.choices[0].delta.content
     last_item = all_items[-1]
 
-    assert generated_text == 'Deep learning is a subfield of machine learning that'
+    assert generated_text == "Deep learning is a subfield of machine learning that"
 
     # Last item has a finish reason
     assert last_item.choices[0].finish_reason == "length"
