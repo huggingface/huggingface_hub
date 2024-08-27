893e88958cbc0ce0bcae4b20b8aa7cb437943c1e
Luis Blanche <loulouserr@gmail.com>
2024-08-27 13:08:03 +0200

    fix secrets inference endpoints (#2490)
    * feat: :sparkles: allow passing secrets through the inference endpoint client
    
    added the secrets argument to create_inference_endpoint, update_inference_endpoint and InferenceEndpoint.update
    
    * test: :white_check_mark: add secrets to test
    
    * Apply suggestions from code review
    
    * fix: :bug: fix: allow not passing secrets at creation
    
    * fix: :bug: remove default secret
    
    ---------
    
    Co-authored-by: Lucain <lucainp@gmail.com>

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -7555,7 +7555,6 @@ class HfApi:
                 "revision": revision,
                 "task": task,
                 "image": image,
-                "secrets": secrets,
             },
             "name": name,
             "provider": {
@@ -7564,7 +7563,8 @@ class HfApi:
             },
             "type": type,
         }
-
+        if secrets:
+            payload["model"]["secrets"] = secrets
         response = get_session().post(
             f"{constants.INFERENCE_ENDPOINTS_ENDPOINT}/endpoint/{namespace}",
             headers=self._build_hf_headers(token=token),
