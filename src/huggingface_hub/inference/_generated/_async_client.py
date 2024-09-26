48c6c5e7b9239ca77367714fe6f19cdb912cc7cd
Lucain Pouget <lucainp@gmail.com>
2024-09-26 09:16:18 +0200

    Document vision chat completion with Llama 3.2 11B V

--- src/huggingface_hub/inference/_generated/_async_client.py
+++ src/huggingface_hub/inference/_generated/_async_client.py
@@ -754,7 +754,7 @@ class AsyncInferenceClient:
         ...     base64_image = base64.b64encode(f.read()).decode("utf-8")
         >>> image_url = f"data:image/jpeg;base64,{base64_image}"
 
-        >>> client = AsyncInferenceClient("HuggingFaceM4/idefics2-8b-chatty")
+        >>> client = AsyncInferenceClient("meta-llama/Llama-3.2-11B-Vision-Instruct")
         >>> output = await client.chat.completions.create(
         ...     messages=[
         ...         {
@@ -773,7 +773,7 @@ class AsyncInferenceClient:
         ...     ],
         ... )
         >>> output
-        A determine figure of Lady Liberty stands tall, holding a torch aloft, atop a pedestal on an island.
+        The image depicts the iconic Statue of Liberty situated in New York Harbor, New York, on a clear day.
         ```
 
         Example using tools:
