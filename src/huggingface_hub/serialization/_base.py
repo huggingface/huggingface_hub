93afb2907c9031bda93ad4571bdc0c0b4d176f52
Marc Sun <marc@huggingface.co>
2024-10-07 18:48:35 +0200

    style

--- src/huggingface_hub/serialization/_base.py
+++ src/huggingface_hub/serialization/_base.py
@@ -107,7 +107,7 @@ def split_state_dict_into_shards_factory(
         # If a `tensor` shares the same underlying storage as another tensor, we put `tensor` in the same `block`
         storage_id = get_storage_id(tensor)
         # We skip tensors on meta, otherwise it will skip the tensor
-        if storage_id is not None and getattr(storage_id[0],"type", None) != 'meta':
+        if storage_id is not None and getattr(storage_id[0], "type", None) != "meta":
             if storage_id in storage_id_to_tensors:
                 # We skip this tensor for now and will reassign to correct shard later
                 storage_id_to_tensors[storage_id].append(key)
