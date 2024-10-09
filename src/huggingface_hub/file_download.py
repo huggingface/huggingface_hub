4058e1f97ebe256b2f3006d4bc31be275c66df6b
Lucain <lucain@huggingface.co>
2024-10-09 12:13:46 +0200

    Fix PermissionError while creating '.no_exist/' directory in cache (#2594)

--- src/huggingface_hub/file_download.py
+++ src/huggingface_hub/file_download.py
@@ -1382,8 +1382,8 @@ def _get_metadata_or_catch_error(
                     commit_hash = http_error.response.headers.get(constants.HUGGINGFACE_HEADER_X_REPO_COMMIT)
                     if commit_hash is not None:
                         no_exist_file_path = Path(storage_folder) / ".no_exist" / commit_hash / relative_filename
-                        no_exist_file_path.parent.mkdir(parents=True, exist_ok=True)
                         try:
+                            no_exist_file_path.parent.mkdir(parents=True, exist_ok=True)
                             no_exist_file_path.touch()
                         except OSError as e:
                             logger.error(
