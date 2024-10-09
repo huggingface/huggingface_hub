09ff14c595c795a24acaa1a1ac988152d3da88e7
Marc Sun <marc@huggingface.co>
2024-10-09 18:18:49 +0200

    update doc

--- src/huggingface_hub/serialization/_torch.py
+++ src/huggingface_hub/serialization/_torch.py
@@ -372,10 +372,10 @@ def get_torch_storage_id(tensor: "torch.Tensor") -> Optional[Tuple["torch.device
     """
     Return unique identifier to a tensor storage.
 
-    Multiple different tensors can share the same underlying storage. For
-    example, "meta" tensors all share the same storage, and thus their identifier will all be equal. This identifier is
+    Multiple different tensors can share the same underlying storage. This identifier is
     guaranteed to be unique and constant for this tensor's storage during its lifetime. Two tensor storages with
     non-overlapping lifetimes may have the same id.
+    In the case of meta tensors, we return None since we are not can't to tell if they share the same storage.
 
     Taken from https://github.com/huggingface/transformers/blob/1ecf5f7c982d761b4daaa96719d162c324187c64/src/transformers/pytorch_utils.py#L278.
     """
