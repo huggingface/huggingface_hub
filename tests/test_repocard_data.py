b15a174cd79a9a7127a9d533b1e38f5d16b63b20
Lucain Pouget <lucainp@gmail.com>
2024-10-22 16:39:49 +0200

    style

--- tests/test_repocard_data.py
+++ tests/test_repocard_data.py
@@ -238,18 +238,17 @@ class ModelCardDataTest(unittest.TestCase):
         assert data.tags == ["tag2", "tag1", "tag3"]
 
     def test_remove_top_level_none_values(self):
-        as_obj = ModelCardData(tags=["tag1", None], foo={"bar":3, "baz": None}, pipeline_tag=None)
+        as_obj = ModelCardData(tags=["tag1", None], foo={"bar": 3, "baz": None}, pipeline_tag=None)
         as_dict = as_obj.to_dict()
 
-
         assert as_obj.tags == ["tag1", None]
-        assert as_dict["tags"] == ["tag1", None] # none value inside list should be kept
+        assert as_dict["tags"] == ["tag1", None]  # none value inside list should be kept
 
-        assert as_obj.foo == {"bar":3, "baz": None}
-        assert as_dict["foo"] == {"bar":3, "baz": None} # none value inside dict should be kept
+        assert as_obj.foo == {"bar": 3, "baz": None}
+        assert as_dict["foo"] == {"bar": 3, "baz": None}  # none value inside dict should be kept
 
         assert as_obj.pipeline_tag is None
-        assert "pipeline_tag" not in as_dict # top level none value should be removed
+        assert "pipeline_tag" not in as_dict  # top level none value should be removed
 
 
 class DatasetCardDataTest(unittest.TestCase):
