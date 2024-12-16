c4e60791799d85e97a7d6c213e3bca407abc373a
Célina <hanouticelina@gmail.com>
2024-12-16 12:24:17 +0100

    [InferenceClient] Keep `generate_kwargs` instead of `generation_parameters` (#2711)
    * deprecate instead of removing generate_kwargs
    
    * remove generation_parameters
    
    * remove generation_parameters from other tasks

--- src/huggingface_hub/inference/_generated/types/text_to_speech.py
+++ src/huggingface_hub/inference/_generated/types/text_to_speech.py
@@ -76,7 +76,8 @@ class TextToSpeechGenerationParameters(BaseInferenceType):
 class TextToSpeechParameters(BaseInferenceType):
     """Additional inference parameters for Text To Speech"""
 
-    generation_parameters: Optional[TextToSpeechGenerationParameters] = None
+    # Will be deprecated in the future when the renaming to `generation_parameters` is implemented in transformers
+    generate_kwargs: Optional[TextToSpeechGenerationParameters] = None
     """Parametrization of the text generation process"""
 
 
