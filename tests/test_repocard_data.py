c1d50d6f7303cc777ac88b8085cadcb436df64a7
Celina Hanouti <hanouticelina@gmail.com>
2025-01-23 10:12:08 +0100

    fix getter in CardData

--- tests/test_repocard_data.py
+++ tests/test_repocard_data.py
@@ -63,6 +63,13 @@ class BaseCardDataTest(unittest.TestCase):
         self.assertTrue("foo" in metadata)
         self.assertFalse("FOO" in metadata)
 
+        # default value
+        self.assertEqual(metadata.get("FOO", "default"), "default")
+        # Should return default when key is in metadata but value is None
+        metadata.FOO = None
+        self.assertEqual(metadata.get("FOO", "default"), "default")
+        # Should return default when key is not in metadata
+        self.assertEqual(metadata.get("BAR", "default"), "default")
         # export
         self.assertEqual(str(metadata), "foo: BAR")
 
