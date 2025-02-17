f700201b0bcab75bfc73bdc33c3bb3afdd7f9dae
Hugo Larcher <hugo.larcher@huggingface.co>
2025-02-17 16:25:43 +0100

    fix: move HF_HUB_USER_AGENT_ORIGIN login to _http_user_agent() following PR comment.

--- src/huggingface_hub/utils/_headers.py
+++ src/huggingface_hub/utils/_headers.py
@@ -125,16 +125,6 @@ def build_hf_headers(
     # Get auth token to send
     token_to_send = get_token_to_send(token)
 
-    # Retrieve user-agent origin headers from environment variable
-    origin = constants.HF_HUB_USER_AGENT_ORIGIN
-    if origin is not None:
-        if isinstance(user_agent, dict):
-            user_agent["origin"] = origin
-        elif isinstance(user_agent, str):
-            user_agent += "; " + "origin/" + origin
-        else:
-            user_agent = {"origin": origin}
-
     # Combine headers
     hf_headers = {
         "user-agent": _http_user_agent(
@@ -222,6 +212,11 @@ def _http_user_agent(
         ua += "; " + "; ".join(f"{k}/{v}" for k, v in user_agent.items())
     elif isinstance(user_agent, str):
         ua += "; " + user_agent
+
+    # Retrieve user-agent origin headers from environment variable
+    origin = constants.HF_HUB_USER_AGENT_ORIGIN
+    if origin is not None:
+        ua += "; origin/" + origin
 
     return _deduplicate_user_agent(ua)
 
