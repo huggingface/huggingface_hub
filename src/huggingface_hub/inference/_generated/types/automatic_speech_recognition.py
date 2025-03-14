454787aee42b7c5f5038d9da8c69c2a3311bd4a1
Celina Hanouti <hanouticelina@gmail.com>
2025-03-14 15:32:54 +0100

    keep generate_kwargs

--- src/huggingface_hub/inference/_generated/types/automatic_speech_recognition.py
+++ src/huggingface_hub/inference/_generated/types/automatic_speech_recognition.py
@@ -79,6 +79,9 @@ class AutomaticSpeechRecognitionParameters(BaseInferenceType):
     """Parametrization of the text generation process"""
     return_timestamps: Optional[bool] = None
     """Whether to output corresponding timestamps with the generated text"""
+    # Will be deprecated in the future when the renaming to `generation_parameters` is implemented in transformers
+    generate_kwargs: Optional[AutomaticSpeechRecognitionGenerationParameters] = None
+    """Parametrization of the text generation process"""
 
 
 @dataclass_with_extra
