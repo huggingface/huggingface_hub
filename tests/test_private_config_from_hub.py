0b188ba209386ae5b3a1979627ba25ba456eb2dd
Brian Ronan <brian.ronan@huggingface.co>
2025-03-27 13:33:40 -0700

    Validating our usePrivateLink is correct for the environment

--- tests/test_private_config_from_hub.py
+++ tests/test_private_config_from_hub.py
@@ -1,3 +1,7 @@
+import base64
+import json
+from huggingface_hub.constants import HUGGINGFACE_HEADER_X_XET_ACCESS_TOKEN
+
 from huggingface_hub.file_download import (
     hf_hub_url,
     _request_wrapper,
@@ -19,15 +23,33 @@ class TestXetPrivateLinks:
 
         r = _request_wrapper(method="HEAD", url=url, headers=headers, allow_redirects=False, follow_relative_redirects=True)
 
-        assert r.status_code == 200
-        assert r.headers[HUGGINGFACE_HEADER_X_XET_ACCESS_TOKEN] == ""
+        assert r.status_code == 302 
 
-def test_check_private_link_token_item_for_hub_ci(self):
+        xet_token = r.headers[HUGGINGFACE_HEADER_X_XET_ACCESS_TOKEN]
+
+        parts = xet_token.split(".")
+        assert len(parts) == 3
+        json_string = base64.b64decode(parts[1] + '==')
+
+        token_info = json.loads(json_string)
+        assert token_info["userId"] == "public"
+        assert token_info["usePrivateLink"] == True 
+
+
+def test_check_private_link_token_item_for_hub_ci():
     url = hf_hub_url("__DUMMY_TRANSFORMERS_USER__/test-list-xet-files", filename="lfs_file.bin")
 
     headers = build_hf_headers()
 
     r = _request_wrapper(method="HEAD", url=url, headers=headers, allow_redirects=False, follow_relative_redirects=True)
 
-    assert r.status_code == 200
-    assert r.headers[HUGGINGFACE_HEADER_X_XET_ACCESS_TOKEN] == ""
+    assert r.status_code == 302 
+    xet_token = r.headers[HUGGINGFACE_HEADER_X_XET_ACCESS_TOKEN]
+
+    parts = xet_token.split(".")
+    assert len(parts) == 3
+    json_string = base64.b64decode(parts[1] + '==')
+
+    token_info = json.loads(json_string)
+    assert token_info["userId"] == "public"
+    assert token_info["usePrivateLink"] == True 
