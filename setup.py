a66d87effb2aea8c92ff0013eb5c67d15fb24a5a
Hugging Face Bot (Xet RC Testing) <bot+xet@huggingface.co>
2025-04-29 20:46:03 +0000

    Test hfh 1.1.0rc0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_version() -> str:
 install_requires = [
     "filelock",
     "fsspec>=2023.5.0",
-    "hf-xet>=1.0.2,<2.0.0; platform_machine=='x86_64' or platform_machine=='amd64' or platform_machine=='arm64' or platform_machine=='aarch64'",
+    "hf_xet==1.1.0rc0; platform_machine=='x86_64' or platform_machine=='amd64' or platform_machine=='arm64' or platform_machine=='aarch64'",
     "packaging>=20.9",
     "pyyaml>=5.1",
     "requests",
@@ -56,7 +56,7 @@ extras["tensorflow-testing"] = [
     "keras<3.0",
 ]
 
-extras["hf_xet"] = ["hf_xet>=1.0.2,<2.0.0"]
+extras["hf_xet"] = ["hf_xet==1.1.0rc0"]
 
 extras["testing"] = (
     extras["cli"]
