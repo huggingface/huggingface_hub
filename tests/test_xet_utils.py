0b5838dc89c701c55e8489cc2e37bb9f5bb6e79a
Rajat Arya <rajat@huggingface.co>
2025-04-29 11:59:28 -0700

    Add logger.info() for using hf-xet for upload

--- tests/test_xet_utils.py
+++ tests/test_xet_utils.py
@@ -2,7 +2,6 @@ import os
 from unittest.mock import MagicMock
 
 import pytest
-import warnings
 
 from huggingface_hub import constants
 from huggingface_hub.utils._xet import (
@@ -263,6 +262,7 @@ def test_env_var_hf_hub_disable_xet() -> None:
     os.environ["HF_HUB_DISABLE_XET"] = "1"
     assert not is_xet_available()
     del os.environ["HF_HUB_DISABLE_XET"]  # Clean up after the test
+
 
 def test_old_hfhub_version_xet(mocker) -> None:
     """Test that old hf-hub results in is_xet_available() emitting a UserWarning."""
