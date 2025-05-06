1e40c62005cb7e3a37ecc4116f80a3883df832ab
célina <hanouticelina@gmail.com>
2025-05-06 12:41:25 +0200

    [Inference Providers] fix inference with URL endpoints (#3041)
    * fix inference with url endpoints
    
    * style
    
    * parentheses
    
    * add test
    
    * Update tests/test_inference_client.py
    
    Co-authored-by: Lucain <lucain@huggingface.co>
    
    * Update tests/test_inference_client.py
    
    Co-authored-by: Lucain <lucain@huggingface.co>
    
    * Update tests/test_inference_client.py
    
    ---------
    
    Co-authored-by: Lucain <lucain@huggingface.co>

--- src/huggingface_hub/inference/_providers/__init__.py
+++ src/huggingface_hub/inference/_providers/__init__.py
@@ -147,7 +147,9 @@ def get_provider_helper(
         ValueError: If provider or task is not supported
     """
 
-    if model is None and provider in (None, "auto"):
+    if (model is None and provider in (None, "auto")) or (
+        model is not None and model.startswith(("http://", "https://"))
+    ):
         provider = "hf-inference"
 
     if provider is None:
