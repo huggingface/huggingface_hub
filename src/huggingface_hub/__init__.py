2e5dc967394819001d19a4c29439884b47d179e5
Celina Hanouti <hanouticelina@gmail.com>
2025-05-13 11:45:11 +0200

    Release: v0.31.2

--- src/huggingface_hub/__init__.py
+++ src/huggingface_hub/__init__.py
@@ -46,7 +46,7 @@ import sys
 from typing import TYPE_CHECKING
 
 
-__version__ = "0.31.1"
+__version__ = "0.31.2"
 
 # Alphabetical order of definitions is ensured in tests
 # WARNING: any comment added in this dictionary definition will be lost when
