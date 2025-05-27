6be1c9d88380bc4b219594e51145cba57bd891f7
Lucain Pouget <lucainp@gmail.com>
2025-05-27 14:05:53 +0200

    Make hf-xet more silent

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -4482,7 +4482,7 @@ class HfApi:
             isinstance(addition.path_or_fileobj, io.BufferedIOBase) for addition in new_lfs_additions_to_upload
         )
         if xet_enabled and not has_buffered_io_data and is_xet_available():
-            logger.info("Uploading files using Xet Storage..")
+            logger.debug("Uploading files using Xet Storage..")
             _upload_xet_files(**upload_kwargs, create_pr=create_pr)  # type: ignore [arg-type]
         else:
             if xet_enabled and is_xet_available():
