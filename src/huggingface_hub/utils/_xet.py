424d97d1db5bcbf0d37196be1fb93e978951af4a
Celina Hanouti <hanouticelina@gmail.com>
2025-06-25 17:04:47 +0200

    simpler

--- src/huggingface_hub/utils/_xet.py
+++ src/huggingface_hub/utils/_xet.py
@@ -55,8 +55,7 @@ def parse_xet_file_data_from_response(
     except KeyError:
         return None
     endpoint = endpoint if endpoint is not None else constants.ENDPOINT
-    if refresh_route.startswith("https://huggingface.co"):
-        refresh_route = refresh_route.replace("https://huggingface.co", endpoint)
+    refresh_route = refresh_route.replace("https://huggingface.co", endpoint)
     return XetFileData(
         file_hash=file_hash,
         refresh_route=refresh_route,
