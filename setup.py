1fc1281bb04161cd86c2b975c162ea19a14d0579
Lucain Pouget <lucainp@gmail.com>
2025-09-10 17:50:24 +0200

    bump pytest

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ extras["testing"] = (
     + [
         "jedi",
         "Jinja2",
-        "pytest>=8.1.1,<8.2.2",  # at least until 8.2.3 is released with https://github.com/pytest-dev/pytest/pull/12436
+        "pytest>=8.4.2",  # we need https://github.com/pytest-dev/pytest/pull/12436
         "pytest-cov",
         "pytest-env",
         "pytest-xdist",
