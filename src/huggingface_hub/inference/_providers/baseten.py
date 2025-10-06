24b38ba2631ca7d5d74d9318e57ab7907065188a
Lucain <lucainp@gmail.com>
2025-10-06 09:19:08 +0200

    Update src/huggingface_hub/inference/_providers/baseten.py

--- src/huggingface_hub/inference/_providers/baseten.py
+++ src/huggingface_hub/inference/_providers/baseten.py
@@ -4,4 +4,3 @@ from ._common import BaseConversationalTask
 class BasetenConversationalTask(BaseConversationalTask):
     def __init__(self):
         super().__init__(provider="baseten", base_url="https://inference.baseten.co")
-
