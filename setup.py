675e461d1861756f660b0c25c189c7b8e96d0eae
Lucain <lucain@huggingface.co>
2025-10-28 13:47:07 +0100

    Remove aiohttp dependency (#3488)
    * Remove aiohttp dependency
    
    * too fast

--- setup.py
+++ setup.py
@@ -28,10 +28,6 @@ install_requires = [
 
 extras = {}
 
-extras["inference"] = [
-    "aiohttp",  # for AsyncInferenceClient
-]
-
 extras["oauth"] = [
     "authlib>=1.3.2",  # minimum version to include https://github.com/lepture/authlib/pull/644
     "fastapi",
@@ -54,11 +50,10 @@ extras["hf_xet"] = ["hf-xet>=1.1.3,<2.0.0"]
 extras["mcp"] = [
     "mcp>=1.8.0",
     "typer",
-] + extras["inference"]
+]
 
 extras["testing"] = (
-    extras["inference"]
-    + extras["oauth"]
+    extras["oauth"]
     + [
         "jedi",
         "Jinja2",
