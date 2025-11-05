15cf873c39c84f19880ba77c1f5d48c19e54d116
Lucain <lucain@huggingface.co>
2025-11-05 10:15:59 +0100

    Upload `hf` CLI to PyPI (#3511)
    * Upload  CLI to PyPI
    
    * update readme

---
+++ utils/hf/hf/__init__.py
@@ -0,0 +1,5 @@
+raise ImportError(
+    "The 'hf' package only provides a CLI entry point. "
+    "It cannot be imported. Please install 'huggingface_hub' instead:\n\n"
+    "    pip install huggingface_hub\n"
+)
