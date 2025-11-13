ff333c3b2b314a93b03ed6331747ea85d45ae994
Raphael Glon <oOraph@users.noreply.github.com>
2025-11-13 11:08:42 +0100

    check ci
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# do not commit just checking the ci on main
 from __future__ import annotations
 
 import inspect
