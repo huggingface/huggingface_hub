31a10ee37df5862f113a077b2ce7b6e5d4fae6b1
Lucain Pouget <lucainp@gmail.com>
2026-01-06 12:01:00 +0100

    Release: v1.2.4

--- src/huggingface_hub/__init__.py
+++ src/huggingface_hub/__init__.py
@@ -46,7 +46,7 @@ import sys
 from typing import TYPE_CHECKING
 
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 # Alphabetical order of definitions is ensured in tests
 # WARNING: any comment added in this dictionary definition will be lost when
