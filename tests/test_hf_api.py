a2362d650334abd2c15eebb092ded91a9266a0f7
Lucain <lucain@huggingface.co>
2026-01-26 15:01:10 +0100

    Apply suggestion from @Wauplin

--- tests/test_hf_api.py
+++ tests/test_hf_api.py
@@ -2109,7 +2109,6 @@ class HfApiPublicProductionTest(unittest.TestCase):
         assert mock_paginate.call_args_list[0][1]["params"] == {"benchmark": ["benchmark:official"]}
         assert mock_paginate.call_args_list[1][1]["params"] == {"benchmark": ["benchmark:official"]}
 
-
     def test_filter_datasets_by_language_creator(self):
         datasets = list(self._api.list_datasets(language_creators="crowdsourced"))
         assert len(datasets) > 0
