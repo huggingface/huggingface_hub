0f17dc58dbe37f31cd037cc3cc11c0d97dd17314
di <di@huggingface.co>
2026-02-05 13:45:30 -0800

    parse xet hash

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -648,6 +648,8 @@ class RepoFile:
             The file's git OID.
         lfs (`BlobLfsInfo`, *optional*):
             The file's LFS metadata.
+        xet_hash (`str`, *optional*):
+            The file's Xet hash.
         last_commit (`LastCommitInfo`, *optional*):
             The file's last commit metadata. Only defined if [`list_repo_tree`] and [`get_paths_info`]
             are called with `expand=True`.
@@ -660,6 +662,7 @@ class RepoFile:
     size: int
     blob_id: str
     lfs: Optional[BlobLfsInfo] = None
+    xet_hash: Optional[str] = None
     last_commit: Optional[LastCommitInfo] = None
     security: Optional[BlobSecurityInfo] = None
 
@@ -671,6 +674,7 @@ class RepoFile:
         if lfs is not None:
             lfs = BlobLfsInfo(size=lfs["size"], sha256=lfs["oid"], pointer_size=lfs["pointerSize"])
         self.lfs = lfs
+        self.xet_hash = kwargs.pop("xetHash", None)
         last_commit = kwargs.pop("lastCommit", None) or kwargs.pop("last_commit", None)
         if last_commit is not None:
             last_commit = LastCommitInfo(
