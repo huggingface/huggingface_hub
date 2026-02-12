1dbd9ccf6cd29490d638ebc8c5a2e1ef6e61c4e5
cbensimon <charles@huggingface.co>
2026-02-12 17:00:11 +0000

    Fix CLIError hint

--- src/huggingface_hub/cli/spaces.py
+++ src/huggingface_hub/cli/spaces.py
@@ -223,9 +223,9 @@ def spaces_hot_reload(
             enable_progress_bars()
         editor_res = _cli_utils.editor_open(filepath)
         if editor_res == "no-tty":
-            raise CLIError("Cannot open an editor (no TTY). Use --local flag to ho-reload from local path")
+            raise CLIError("Cannot open an editor (no TTY). Use -f flag to ho-reload from local path")
         if editor_res == "no-editor":
-            raise CLIError("No editor found in local environment. Use --local flag to ho-reload from local path")
+            raise CLIError("No editor found in local environment. Use -f flag to ho-reload from local path")
         if editor_res != 0:
             raise CLIError(f"Editor returned a non-zero exit code while attempting to edit {filepath}")
     else:
