34b6cb9bce11fdc2d67da8ff0344b2ed2ca34930
Lucain Pouget <lucainp@gmail.com>
2026-02-16 15:43:33 +0100

    fix test

--- src/huggingface_hub/hf_api.py
+++ src/huggingface_hub/hf_api.py
@@ -4807,7 +4807,7 @@ class HfApi:
             # If `create_pr`, we don't want to check user permission on the revision as users with read permission
             # should still be able to create PRs even if they don't have write permission on the target branch of the
             # PR (i.e. `revision`).
-            "revision": revision if not create_pr else None,
+            "revision": revision or constants.DEFAULT_REVISION if not create_pr else constants.DEFAULT_REVISION,
         }
         _upload_files(**upload_kwargs, num_threads=num_threads, create_pr=create_pr)  # type: ignore [arg-type]
         for addition in new_lfs_additions_to_upload:
