4f400e933acc1014c1ba3e2ffa4b449944c5dfba
cbensimon <charles@huggingface.co>
2026-02-19 14:45:08 +0000

    Put \b back

--- src/huggingface_hub/cli/spaces.py
+++ src/huggingface_hub/cli/spaces.py
@@ -181,6 +181,7 @@ def spaces_hot_reload(
 
     Usage examples:
 
+        \b
         hf spaces hot-reload username/repo-name app.py               # Open an interactive editor to the remote app.py file
         hf spaces hot-reload username/repo-name -f app.py            # Take local version from ./app.py and patch app.py in remote repo
         hf spaces hot-reload username/repo-name app.py -f src/app.py # Take local version from ./src/app.py and patch app.py in remote repo
