8d04d7ffac0a6b4e6546535c17e55e007a16b80e
textclf <contact@textclf.com>
2026-03-07 19:48:27 -0800

    [Inference Provider] Add TextCLF as an inference provider

--- src/huggingface_hub/inference/_providers/textclf.py
+++ src/huggingface_hub/inference/_providers/textclf.py
@@ -14,9 +14,6 @@ class TextCLFTextGenerationTask(BaseTextGenerationTask):
     def __init__(self):
         super().__init__(provider=_PROVIDER, base_url=_BASE_URL)
 
-    def _prepare_route(self, mapped_model: str, api_key: str) -> str:
-        return "/v1/chat/completions"
-
     def get_response(self, response: Union[bytes, dict], request_params: Optional[RequestParameters] = None) -> Any:
         output = _as_dict(response)["choices"][0]
         return {
@@ -31,6 +28,3 @@ class TextCLFTextGenerationTask(BaseTextGenerationTask):
 class TextCLFConversationalTask(BaseConversationalTask):
     def __init__(self):
         super().__init__(provider=_PROVIDER, base_url=_BASE_URL)
-
-    def _prepare_route(self, mapped_model: str, api_key: str) -> str:
-        return "/v1/chat/completions"
